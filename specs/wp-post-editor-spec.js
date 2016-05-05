837f6d0da7e72a5287d687af4487b5b94b391171
Alister Scott <alister.scott@automattic.com>
2016-05-05 18:47:49 +1000

    Added new spec 'Edit a Post'

--- specs/wp-post-editor-spec.js
+++ specs/wp-post-editor-spec.js
@@ -9,7 +9,10 @@ import TwitterFeedPage from '../lib/pages/twitter-feed-page.js';
 import ViewPostPage from '../lib/pages/view-post-page.js';
 import NotFoundPage from '../lib/pages/not-found-page.js';
 import ReaderPage from '../lib/pages/reader-page.js';
+import PostsPage from '../lib/pages/posts-page.js';
 
+import SidebarComponent from '../lib/components/sidebar-component.js';
+import NavbarComponent from '../lib/components/navbar-component.js';
 import PostPreviewComponent from '../lib/components/post-preview-component.js';
 import PostEditorSidebarComponent from '../lib/components/post-editor-sidebar-component.js';
 
@@ -656,6 +659,91 @@ test.describe( 'Editor: Posts (' + screenSize + ')', function() {
 				const readerPage = new ReaderPage( driver );
 				readerPage.displayed().then( ( displayed ) => {
 					assert.equal( displayed, true, 'The reader page is not displayed' );
+				} );
+			} );
+		} );
+	} );
+
+	test.describe( 'Edit a Post:', function() {
+		this.bailSuite( true );
+
+		test.it( 'Delete Cookies and Local Storage', function() {
+			driverManager.clearCookiesAndDeleteLocalStorage( driver );
+		} );
+
+		test.describe( 'Publish a New Post', function() {
+			const originalBlogPostTitle = dataHelper.randomPhrase();
+			const updatedBlogPostTitle = dataHelper.randomPhrase();
+			const blogPostQuote = 'Science is organised knowledge. Wisdom is organised life..\n~ Immanuel Kant\n';
+
+			test.it( 'Can log in', function() {
+				this.loginFlow = new LoginFlow( driver );
+				return this.loginFlow.loginAndStartNewPost();
+			} );
+
+			test.it( 'Can enter post title and content', function() {
+				this.editorPage = new EditorPage( driver );
+				this.editorPage.enterTitle( originalBlogPostTitle );
+				this.editorPage.enterContent( blogPostQuote );
+				return this.editorPage.errorDisplayed().then( ( errorShown ) => {
+					return assert.equal( errorShown, false, 'There is an error shown on the editor page!' );
+				} );
+			} );
+
+			test.it( 'Can publish the post', function() {
+				this.postEditorSidebarComponent = new PostEditorSidebarComponent( driver );
+				this.postEditorSidebarComponent.ensureSaved();
+				this.postEditorSidebarComponent.publishPost();
+				return this.postEditorSidebarComponent.waitForSuccessViewPostNotice();
+			} );
+
+			test.describe( 'Edit the post via posts', function() {
+				test.it( 'Can view the posts list', function() {
+					this.navbarComponent = new NavbarComponent( driver );
+					this.navbarComponent.clickMySites();
+					this.sidebarComponent = new SidebarComponent( driver );
+					this.sidebarComponent.selectPosts();
+					this.postsPage = new PostsPage( driver );
+					this.postsPage.waitForPosts();
+				} );
+
+				test.it( 'Can see and edit our new post', function() {
+					this.postsPage.isPostDisplayed( originalBlogPostTitle ).then( ( displayed ) => {
+						assert.equal( displayed, true, `The blog post titled '${originalBlogPostTitle}' is not displayed in the list of posts` );
+					} );
+					this.postsPage.editPostWithTitle( originalBlogPostTitle );
+					this.editorPage = new EditorPage( driver );
+				} );
+
+				test.it( 'Can see the post title', function() {
+					this.editorPage.waitForTitle();
+					this.editorPage.titleShown().then( ( titleShown ) => {
+						assert.equal( titleShown, originalBlogPostTitle, 'The blog post title shown was unexpected' );
+					} );
+				} );
+
+				test.it( 'Can set the new title and save it', function() {
+					this.editorPage.enterTitle( updatedBlogPostTitle );
+					this.editorPage.errorDisplayed().then( ( errorShown ) => {
+						assert.equal( errorShown, false, 'There is an error shown on the editor page!' );
+					} );
+					this.postEditorSidebarComponent = new PostEditorSidebarComponent( driver );
+					this.postEditorSidebarComponent.ensureSaved();
+					this.postEditorSidebarComponent.publishPost();
+					return this.postEditorSidebarComponent.waitForSuccessViewPostNotice();
+				} );
+
+				test.describe( 'Can view the post with the new title', function() {
+					test.it( 'Can view the post', function() {
+						this.postEditorSidebarComponent.viewPublishedPostOrPage();
+						return this.viewPostPage = new ViewPostPage( driver );
+					} );
+
+					test.it( 'Can see correct post title', function() {
+						this.viewPostPage.postTitle().then( function( postTitle ) {
+							assert.equal( postTitle.toLowerCase(), updatedBlogPostTitle.toLowerCase(), 'The published blog post title is not correct' );
+						} );
+					} );
 				} );
 			} );
 		} );
