fb32b6ab3962f2dfce13adbfe20b2fadf86eb21c
Scott Stancil <hoverduck@users.noreply.github.com>
2016-06-23 10:02:11 -0400

    Add live branch option (#76)
    Adds an option to run the test suite against a live branch hosted on https://calypso.live
    
    The live branch query string is appended in three places:
    1. Reader Page (root page) on visit
    2. Start Page (for signups) on visit
    3. Accept Invite Page on visit

--- run.sh
+++ run.sh
@@ -6,21 +6,26 @@ JOBS=0
 AFTER="lib/after.js"
 OPTS=""
 SCREENSIZES="mobile,desktop,tablet"
+BRANCH=""
 VISDIFF=0
 RETURN=0
 
-I18N_CONFIG="--NODE_CONFIG='{\"browser\":\"firefox\",\"proxy\":\"system\",\"neverSaveScreenshots\":\"true\"}'"
-VISDIFF_CONFIG="--NODE_CONFIG='{\"browser\":\"firefox\", \"proxy\":\"system\", \"neverSaveScreenshots\":\"true\"}'"
+# Function to join arrays into a string
+function joinStr { local IFS="$1"; shift; echo "$*"; }
+
+I18N_CONFIG="\"browser\":\"firefox\",\"proxy\":\"system\",\"neverSaveScreenshots\":\"true\""
+VISDIFF_CONFIG="\"browser\":\"firefox\", \"proxy\":\"system\", \"neverSaveScreenshots\":\"true\""
 declare -a TARGETS
 
 usage () {
   cat <<EOF
 -R		  - Use custom Slack/Spec/XUnit reporter, otherwise just use Spec reporter
 -p 		  - Execute the tests in parallel via CircleCI envvars (implies -g -s mobile,desktop,tablet)
+-b [branch]	  - Run tests on given branch via https://calypso.live
 -s		  - Screensizes in a comma-separated list (defaults to mobile,desktop,tablet)
 -g		  - Execute general tests in the specs/ directory
--i		  - Execute i18n tests in the specs-i18n/ directory
--v [all/critical] - Execute the visdiff tests in specs-visdiff[/critical].  Must specify either 'all' or 'critical'.
+-i		  - Execute i18n tests in the specs-i18n/ directory, not compatible with -g flag
+-v [all/critical] - Execute the visdiff tests in specs-visdiff[/critical].  Must specify either 'all' or 'critical'.  Only accessible in combination with -p flag
 -h		  - This help listing
 EOF
   exit 0
@@ -30,7 +35,7 @@ if [ $# -eq 0 ]; then
   usage
 fi
 
-while getopts ":Rps:giv:h" opt; do
+while getopts ":Rpb:s:giv:h" opt; do
   case $opt in
     R)
       REPORTER="-R spec-xunit-slack-reporter"
@@ -38,6 +43,10 @@ while getopts ":Rps:giv:h" opt; do
       ;;
     p)
       PARALLEL=1
+      continue
+      ;;
+    b)
+      NODE_CONFIG_ARGS+=("\"liveBranch\":\"true\",\"branchName\":\"$OPTARG\",\"calypsoBaseURL\":\"https://calypso.live\"")
       continue
       ;;
     s)
@@ -48,18 +57,11 @@ while getopts ":Rps:giv:h" opt; do
       TARGET="specs/"
       ;;
     i)
-      TARGET="$I18N_CONFIG specs-i18n/"
+      NODE_CONFIG_ARGS+=$I18N_CONFIG
+      TARGET="specs-i18n/"
       ;;
     v)
       VISDIFF=1
-      if [ "$OPTARG" == "all" ]; then
-        TARGET="$VISDIFF_CONFIG specs-visdiff/\*"
-      elif [ "$OPTARG" == "critical" ]; then
-        TARGET="$VISDIFF_CONFIG specs-visdiff/critical/"
-      else
-        echo "-v supports the following values: all or critical"
-        exit 1
-      fi
       ;;
     h)
       usage
@@ -82,6 +84,9 @@ done
 # Skip any tests in the given variable
 GREP="-i -g '$SKIP_TEST_REGEX'"
 
+# Combine any NODE_CONFIG entries into a single object
+NODE_CONFIG_ARG="$(joinStr , ${NODE_CONFIG_ARGS[*]})"
+
 if [ $PARALLEL == 1 ]; then
   # Assign an index to each test segment to run in parallel
   MOBILE=$(expr 0 % $CIRCLE_NODE_TOTAL)
@@ -90,33 +95,38 @@ if [ $PARALLEL == 1 ]; then
   VISUAL=$(expr 3 % $CIRCLE_NODE_TOTAL)
   echo "Parallel execution details:"
   echo "mobile=$MOBILE, desktop=$DESKTOP, tablet=$TABLET, visual=$VISUAL, node=$CIRCLE_NODE_INDEX, total=$CIRCLE_NODE_TOTAL"
-  
+
   if [ $CIRCLE_NODE_INDEX == $MOBILE ]; then
       echo "Executing tests at mobile screen width"
-      CMD="env BROWSERSIZE=mobile $MOCHA $GREP $REPORTER specs/ $AFTER"
+      NC="--NODE_CONFIG='{$NODE_CONFIG_ARG}'"
+      CMD="env BROWSERSIZE=mobile $MOCHA $NC $GREP $REPORTER specs/ $AFTER"
 
       eval $CMD
       RETURN+=$?
   fi
   if [ $CIRCLE_NODE_INDEX == $DESKTOP ]; then
       echo "Executing tests at desktop screen width"
-      CMD="env BROWSERSIZE=desktop $MOCHA $GREP $REPORTER specs/ $AFTER"
+      NC="--NODE_CONFIG='{$NODE_CONFIG_ARG}'"
+      CMD="env BROWSERSIZE=desktop $MOCHA $NC $GREP $REPORTER specs/ $AFTER"
 
       eval $CMD
       RETURN+=$?
   fi
   if [ $CIRCLE_NODE_INDEX == $TABLET ]; then
       echo "Executing tests at tablet screen width"
-      CMD="env BROWSERSIZE=tablet $MOCHA $GREP $REPORTER specs/ $AFTER"
+      NC="--NODE_CONFIG='{$NODE_CONFIG_ARG}'"
+      CMD="env BROWSERSIZE=tablet $MOCHA $NC $GREP $REPORTER specs/ $AFTER"
 
       eval $CMD
       RETURN+=$?
   fi
   if [ $CIRCLE_NODE_INDEX == $VISUAL ] && [ $VISDIFF == 1 ]; then
       echo "Executing visdiff tests at all screen widths"
-      CMD1="env BROWSERSIZE=mobile $MOCHA $GREP $REPORTER $VISDIFF_CONFIG specs-visdiff/critical/ $AFTER"
-      CMD2="env BROWSERSIZE=desktop $MOCHA $GREP $REPORTER $VISDIFF_CONFIG specs-visdiff/critical/ $AFTER"
-      CMD3="env BROWSERSIZE=tablet $MOCHA $GREP $REPORTER $VISDIFF_CONFIG specs-visdiff/critical/ $AFTER"
+      NC="--NODE_CONFIG='{$VISDIFF_CONFIG,$NODE_CONFIG_ARG}'"
+
+      CMD1="env BROWSERSIZE=mobile $MOCHA $NC $GREP $REPORTER $VISDIFF_CONFIG specs-visdiff/critical/ $AFTER"
+      CMD2="env BROWSERSIZE=desktop $MOCHA $NC $GREP $REPORTER $VISDIFF_CONFIG specs-visdiff/critical/ $AFTER"
+      CMD3="env BROWSERSIZE=tablet $MOCHA $NC $GREP $REPORTER $VISDIFF_CONFIG specs-visdiff/critical/ $AFTER"
 
       eval $CMD1
       RETURN+=$?
@@ -126,11 +136,13 @@ if [ $PARALLEL == 1 ]; then
       RETURN+=$?
   fi
 else # Not a parallel run, just queue up the tests in sequence
+  NC="--NODE_CONFIG='{$NODE_CONFIG_ARG}'"
+
   if [ "$CI" != "true" ] || [ $CIRCLE_NODE_INDEX == 0 ]; then
     IFS=, read -r -a SCREENSIZE_ARRAY <<< "$SCREENSIZES"
     for size in ${SCREENSIZE_ARRAY[@]}; do
       for target in "${TARGETS[@]}"; do
-        CMD="env BROWSERSIZE=$size $MOCHA $GREP $REPORTER $target $AFTER"
+        CMD="env BROWSERSIZE=$size $MOCHA $NC $GREP $REPORTER $target $AFTER"
 
         eval $CMD
         RETURN+=$?
