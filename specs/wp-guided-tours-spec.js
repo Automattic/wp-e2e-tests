216985a1d62bc892d1cef4e695df035d926ce898
ehg <chris@ehg.sh>
2016-06-29 11:22:20 +0100

    add GuidedTourFirstStep component; use query args for login
    - the component is specific to the first step atm, this will change
    - using query args for login allows us to avoid a page reload

--- specs/wp-guided-tours-spec.js
+++ specs/wp-guided-tours-spec.js
@@ -1,11 +1,11 @@
 import test from 'selenium-webdriver/testing';
 import config from 'config';
-import assert from 'assert';
-import { By as by } from 'selenium-webdriver';
+import { assert } from 'chai';
 
 import * as driverManager from '../lib/driver-manager.js';
 
 import LoginFlow from '../lib/flows/login-flow.js';
+import GuidedTourFirstStep from '../lib/components/guided-tour-step';
 
 const mochaTimeOut = config.get( 'mochaTimeoutMS' );
 const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
@@ -23,19 +23,15 @@ test.describe( 'Guided Tours: (' + screenSize + ')', function() {
 	this.bailSuite( true );
 
 	test.describe( 'Forcing a tour:', function() {
-		test.before( 'Delete Cookies and Login', function() {
+		test.before( 'Logs in', function() {
 			driverManager.clearCookiesAndDeleteLocalStorage( driver );
 			let loginFlow = new LoginFlow( driver );
-			loginFlow.login();
-			driver.getCurrentUrl().then( ( url ) => driver.get( url + '?tour=main' ) );
+			loginFlow.login( '?tour=main' );
+			this.step = new GuidedTourFirstStep( driver );
 		} );
 
-		test.it( 'Shows the initial step', function( done ) {
-			const step = by.className( 'guided-tours__step-first' );
-			driver.isElementPresent( step ).then( ( present ) => {
-				assert( present, true );
-				done();
-			} );
+		it( 'Shows the initial step', function() {
+			return this.step.isFirstStepShown().then( shown => assert.equal( true, shown ) );
 		} );
 	} );
 } );
