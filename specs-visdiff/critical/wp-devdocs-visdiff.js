97da3afb04babb603ab6b65d946737e91d6aa897
Scott Stancil <scott.stancil@automattic.com>
2016-07-14 11:43:14 -0400

    Cross-browser diffs functional from our side, still pending Applitools issues

--- specs-visdiff/critical/wp-devdocs-visdiff.js
+++ specs-visdiff/critical/wp-devdocs-visdiff.js
@@ -8,12 +8,12 @@ import LoginFlow from '../../lib/flows/login-flow.js';
 
 import DevdocsDesignPage from '../../lib/pages/devdocs-design-page.js';
 
-import webdriver from 'selenium-webdriver';
-const by = webdriver.By;
-
 const mochaVisDiffTimeOut = config.get( 'mochaVisDiffTimeoutMS' );
 const mochaDevDocsTimeOut = config.get( 'mochaDevDocsTimeoutMS' );
 const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
+
+import webdriver from 'selenium-webdriver';
+const by = webdriver.By;
 
 let driver, screenSize, screenSizeName;
 screenSizeName = driverManager.currentScreenSize();
@@ -28,6 +28,7 @@ if ( process.env.CIRCLE_BUILD_NUM ) {
 }
 
 test.before( function() {
+	console.log( 'First test.before() in wp-devdocs-visdiff.js' );
 	this.timeout( startBrowserTimeoutMS );
 	driver = driverManager.startBrowser();
 	screenSize = driverManager.getSizeAsObject();
@@ -35,86 +36,63 @@ test.before( function() {
 
 test.describe( 'DevDocs Visual Diff (' + screenSizeName + ')', function() {
 	var devdocsDesignPage;
+	console.log( 'Beginning DevDocs visdiff' );
 	this.timeout( mochaDevDocsTimeOut );
 
 	test.before( function() {
-		if ( config.has( 'sauce' ) && config.get( 'sauce' ) ) {
-			eyes.setBaselineName( 'cross-browser-devdocs' );
-			eyes.setMatchLevel( 'LAYOUT2' );
-		}
-		eyes.open( driver, 'WordPress.com', 'DevDocs Design [' + screenSizeName + ']', screenSize );
+		var timer;
+
+		console.log( 'Second test.before() in wp-devdocs-visdiff.js, about to open eyes object' );
+		eyes.open( driver, 'WordPress.com', 'DevDocs Design [' + screenSizeName + ']', screenSize ).then( function() {
+			console.log( 'The eyes object is open' );
+		} );
 
 		let loginFlow = new LoginFlow( driver );
-		loginFlow.login();
+		loginFlow.login().then( function() {
+			console.log( 'Logged in' );
+			timer = setTimeout( function() {
+				console.log( 'DevDocs Page failed to load in time, calling window.stop()' );
+				driver.executeScript( 'window.stop()' );
+			}, 45000 );
+		} );
 
 		devdocsDesignPage = new DevdocsDesignPage( driver, true );
-		return devdocsDesignPage.displayed();
+		driver.wait( devdocsDesignPage.displayed(), mochaVisDiffTimeOut ).then( function() {
+			console.log( 'Design Page Loaded' );
+			clearTimeout( timer );
+		} );
 	} );
 
 	test.it( 'Verify UI Components', function() {
-		this.timeout( mochaDevDocsTimeOut * 2 );
+		this.timeout( mochaDevDocsTimeOut );
 		devdocsDesignPage.openUIComponents().then( function() {
-			devdocsDesignPage.getAllDesignElementLinks().then( function( hrefs ) {
-				let flow = driver.controlFlow();
-
-				for ( const href of hrefs ) {
-					let title;
-
-					// Open the design element
-					flow.execute( function() {
-						let titleSplit = href.split( '/' );
-						title = titleSplit[ titleSplit.length - 1 ];
-						return driver.get( href );
-					} );
-//					// Scroll back to the top of the page
-//					flow.execute( function() {
-//						return driver.executeScript( 'window.scrollTo( 0, 0 )' );
-//					} );
-//					// Get the title
-//					flow.execute( function() {
-//						return devdocsDesignPage.getCurrentElementTitle().then( function( _title ) {
-//							title = _title;
-//						} );
-//					} );
-					// Hide the masterbar for clean CSS stitching
-					flow.execute( function() {
-						return devdocsDesignPage.hideMasterbar();
-					} );
-					// Take the screenshot
-					flow.execute( function() {
-						return driverHelper.eyesScreenshot( driver, eyes, title, by.id( 'primary' ) );
-					} );
-//					// Scroll back to the top of the page
-//					flow.execute( function() {
-//						return driver.executeScript( 'window.scrollTo( 0, 0 )' );
-//					} );
-					// Check for Compact button
-					flow.execute( function() {
-						return devdocsDesignPage.isCurrentElementCompactable().then( function( compactable ) {
-							if ( compactable ) {
-								return devdocsDesignPage.getCurrentElementCompactButton().then( function( button ) {
-									// Chrome needs a more precise click on the mobile width to avoid overlapping elements
-									if ( global.browserName.toLowerCase() === 'chrome' ) {
-										return driver.actions().mouseMove( button, {x: 3, y: 3} ).click().perform().then( function() {
-											return driverHelper.eyesScreenshot( driver, eyes, title + ' (Compact)', by.id( 'primary' ) );
-										} );
-									}
-
-									return button.click().then( function() {
-										return driverHelper.eyesScreenshot( driver, eyes, title + ' (Compact)', by.id( 'primary' ) );
-									} );
+			devdocsDesignPage.getAllDesignElements().then( function( elements ) {
+				// Screenshot each element individually to avoid Applitools page size restrictions
+				for ( const el of elements ) {
+					// Retrieve the name of the element, and the "Compact" button if available
+					el.findElements( by.css( 'h2 a' ) ).then( function( anchors ) {
+						// Two anchors means there's a "Compact" option
+						if ( anchors.length === 2 ) {
+							anchors[0].getInnerHtml().then( function( name ) {
+								driverHelper.eyesScreenshot( driver, eyes, name + ' (Full)', el );
+								anchors[1].click().then( function() {
+									driverHelper.eyesScreenshot( driver, eyes, name + ' (Compact)', el );
 								} );
-							}
-						} );
+							} );
+						} else if ( anchors.length === 1 ) {
+							anchors[0].getInnerHtml().then( function( name ) {
+								driverHelper.eyesScreenshot( driver, eyes, name, el );
+							} );
+						} else if ( anchors.length === 0 ) { // wp-calypso issue #4079, but could recur, so I'm leaving the check
+							el.findElement( by.css( 'h2' ) ).then( function( h2 ) {
+								h2.getInnerHtml().then( function( name ) {
+									driverHelper.eyesScreenshot( driver, eyes, name, el );
+								} );
+							} );
+						} else {
+							throw new Error( 'Unexpected number of "h2 a" tags in Design Element' );
+						}
 					} );
-//					// Scroll back to the top of the page
-//					flow.execute( function() {
-//						return driver.executeScript( 'window.scrollTo( 0, 0 )' );
-//					} );
-//					// Return to the main list
-//					flow.execute( function() {
-//						return devdocsDesignPage.returnToAllComponents();
-//					} );
 				}
 			} );
 		} );
@@ -122,17 +100,13 @@ test.describe( 'DevDocs Visual Diff (' + screenSizeName + ')', function() {
 
 	test.it( 'Verify Typography', function() {
 		devdocsDesignPage.openTypography().then( function() {
-			devdocsDesignPage.hideMasterbar().then( function() {
-				driverHelper.eyesScreenshot( driver, eyes, 'DevDocs Design (Typography)', by.id( 'primary' ) );
-			} );
+			driverHelper.eyesScreenshot( driver, eyes, 'DevDocs Design (Typography)' );
 		} );
 	} );
 
 	test.it( 'Verify App Components', function() {
 		devdocsDesignPage.openAppComponents().then( function() {
-			devdocsDesignPage.hideMasterbar().then( function() {
-				driverHelper.eyesScreenshot( driver, eyes, 'DevDocs Design (App Components)', by.id( 'primary' ) );
-			} );
+			driverHelper.eyesScreenshot( driver, eyes, 'DevDocs Design (App Components)' );
 		} );
 	} );
 
