70bd9761c4c5e8761ac260b536ee7137dbacfca8
Alister Scott <alister.scott@gmail.com>
2016-07-28 11:00:12 +1000

    Update post-nux tests to be nested (#164)
    * Update after-hook timeout
    
    * Nest the spec to fail fast

--- specs/wp-post-nux-spec.js
+++ specs/wp-post-nux-spec.js
@@ -125,145 +125,149 @@ test.describe( 'Post-NUX Flows (' + screenSize + ')', function() {
 						const customizerPage = new CustomizerPage( driver );
 						return customizerPage.closeOpenSection();
 					} );
-				} );
 
-				test.describe( 'Customize Base Font', function() {
-					let baseFontName = '';
+					test.describe( 'Customize Base Font', function() {
+						let baseFontName = '';
 
-					test.it( 'Expand fonts', function() {
-						const customizerPage = new CustomizerPage( driver );
-						customizerPage.expandFonts();
-					} );
-
-					test.it( 'Can set the site\'s base font', function() {
-						const customizerPage = new CustomizerPage( driver );
-						return customizerPage.chooseFirstBaseFont().then( ( font ) => {
-							baseFontName = font.toLowerCase().replace( /\s/g, '' );
+						test.it( 'Expand fonts', function() {
+							const customizerPage = new CustomizerPage( driver );
+							customizerPage.expandFonts();
 						} );
-					} );
 
-					test.it( 'Can see the site\'s heading font in preview', function() {
-						const customizerPage = new CustomizerPage( driver );
-						assert( baseFontName !== '', 'The heading font was not retrieved from the customizer' );
-						customizerPage.previewUsesFont( baseFontName ).then( ( fontUsed ) => {
-							assert( fontUsed, `The font '${baseFontName}' does not appear to be used in the customizer preview` );
-						} );
-					} );
-
-					test.it( 'Close custom fonts', function() {
-						const customizerPage = new CustomizerPage( driver );
-						return customizerPage.closeOpenSection();
-					} );
-				} );
-
-				if ( screenSize !== 'mobile' ) { // header images broken on mobile https://github.com/Automattic/wp-calypso/issues/2380
-					test.describe( 'Custom Header Image', function() {
-						let fileDetails = null;
-
-						test.before( 'Create header image file for upload', function() {
-							this.customizerPage = new CustomizerPage( driver );
-							return mediaHelper.createFile().then( function( details ) {
-								fileDetails = details;
+						test.it( 'Can set the site\'s base font', function() {
+							const customizerPage = new CustomizerPage( driver );
+							return customizerPage.chooseFirstBaseFont().then( ( font ) => {
+								baseFontName = font.toLowerCase().replace( /\s/g, '' );
 							} );
 						} );
 
-						test.it( 'Expand header image', function() {
-							this.customizerPage.expandHeaderImage();
-						} );
-
-						test.it( 'Can set a custom header image', function() {
-							this.customizerPage.setHeaderImage( fileDetails );
-							this.customizerPage.waitForPreviewRefresh();
-						} );
-
-						test.it( 'Can see the custom header image in preview', function() {
-							this.customizerPage.previewShowsHeader( fileDetails ).then( ( showsHeader ) => {
-								assert( showsHeader, 'The preview is not showing the recently uploaded header image' );
+						test.it( 'Can see the site\'s heading font in preview', function() {
+							const customizerPage = new CustomizerPage( driver );
+							assert( baseFontName !== '', 'The heading font was not retrieved from the customizer' );
+							customizerPage.previewUsesFont( baseFontName ).then( ( fontUsed ) => {
+								assert( fontUsed, `The font '${baseFontName}' does not appear to be used in the customizer preview` );
 							} );
 						} );
 
-						test.it( 'Close custom header', function() {
-							this.customizerPage.closeOpenSection();
+						test.it( 'Close custom fonts', function() {
+							const customizerPage = new CustomizerPage( driver );
+							return customizerPage.closeOpenSection();
 						} );
 
-						test.after( function() {
-							if ( fileDetails ) {
-								mediaHelper.deleteFile( fileDetails ).then( function() {} );
-							}
+
+						if ( screenSize !== 'mobile' ) { // header images broken on mobile https://github.com/Automattic/wp-calypso/issues/2380
+							test.describe( 'Custom Header Image', function() {
+								let fileDetails = null;
+
+								test.before( 'Create header image file for upload', function() {
+									this.customizerPage = new CustomizerPage( driver );
+									return mediaHelper.createFile().then( function( details ) {
+										fileDetails = details;
+									} );
+								} );
+
+								test.it( 'Expand header image', function() {
+									this.customizerPage.expandHeaderImage();
+								} );
+
+								test.it( 'Can set a custom header image', function() {
+									this.customizerPage.setHeaderImage( fileDetails );
+									this.customizerPage.waitForPreviewRefresh();
+								} );
+
+								test.it( 'Can see the custom header image in preview', function() {
+									this.customizerPage.previewShowsHeader( fileDetails ).then( ( showsHeader ) => {
+										assert( showsHeader, 'The preview is not showing the recently uploaded header image' );
+									} );
+								} );
+
+								test.it( 'Close custom header', function() {
+									this.customizerPage.closeOpenSection();
+								} );
+
+								test.after( function() {
+									if ( fileDetails ) {
+										mediaHelper.deleteFile( fileDetails ).then( function() {} );
+									}
+								} );
+							} );
+						}
+
+						test.describe( 'Add a new menu', function() {
+							const newMenuName = dataHelper.getMenuName();
+
+							test.it( 'Expand menus', function() {
+								this.customizerPage = new CustomizerPage( driver );
+								return this.customizerPage.expandMenus();
+							} );
+
+							test.it( 'Can add a new menu as the primary menu', function() {
+								return this.customizerPage.addNewMenuAndSetAsPrimary( newMenuName );
+							} );
+
+							test.it( 'Can see the new menu listed as primary', function() {
+								return this.customizerPage.menuDisplayedAsPrimary( newMenuName ).then( ( displayed ) => {
+									return assert( displayed, `The menu '${newMenuName}' was not displayed as the primary menu` );
+								} );
+							} );
+
+							test.it( 'Close menus', function() {
+								this.customizerPage.closeOpenPanel();
+							} );
+
+
+							test.describe( 'Add a new widget', function() {
+								const widgetTitle = dataHelper.getWidgetTitle();
+								const widgetContent = dataHelper.getWidgetContent();
+
+								test.it( 'Expand widgets', function() {
+									this.customizerPage = new CustomizerPage( driver );
+									return this.customizerPage.expandWidgets();
+								} );
+
+								test.it( 'Can add a new sidebar widget as a text widget', function() {
+									return this.customizerPage.addNewSidebarTextWidget( widgetTitle, widgetContent );
+								} );
+
+								test.it( 'Can see the new widget in the preview pane', function() {
+									return this.customizerPage.previewShowsWidget( widgetTitle, widgetContent ).then( ( displayed ) => {
+										return assert( displayed, `The widget with title '${widgetTitle}' and content '${widgetContent}' was not displayed in the preview` );
+									} );
+								} );
+
+								test.it( 'Close widgets', function() {
+									this.customizerPage.closeOpenPanel();
+								} );
+
+
+								test.describe( 'Setting static front page', function() {
+									test.it( 'Expand static front page', function() {
+										this.customizerPage = new CustomizerPage( driver );
+										return this.customizerPage.expandStaticFrontPage();
+									} );
+
+									test.it( 'Can see the front page option', function() {
+										return this.customizerPage.frontPageOptionDisplayed();
+									} );
+
+									test.it( 'Can see the posts page option', function() {
+										return this.customizerPage.postsPageOptionDisplayed();
+									} );
+
+									test.it( 'Close front page section', function() {
+										this.customizerPage.closeOpenSection();
+									} );
+
+
+									test.describe( 'Closing the customizer', function() {
+										test.it( 'Close the customizer', function() {
+											const customizerPage = new CustomizerPage( driver );
+											return customizerPage.close();
+										} );
+									} );
+								} );
+							} );
 						} );
-					} );
-				}
-
-				test.describe( 'Add a new menu', function() {
-					const newMenuName = dataHelper.getMenuName();
-
-					test.it( 'Expand menus', function() {
-						this.customizerPage = new CustomizerPage( driver );
-						return this.customizerPage.expandMenus();
-					} );
-
-					test.it( 'Can add a new menu as the primary menu', function() {
-						return this.customizerPage.addNewMenuAndSetAsPrimary( newMenuName );
-					} );
-
-					test.it( 'Can see the new menu listed as primary', function() {
-						return this.customizerPage.menuDisplayedAsPrimary( newMenuName ).then( ( displayed ) => {
-							return assert( displayed, `The menu '${newMenuName}' was not displayed as the primary menu` );
-						} );
-					} );
-
-					test.it( 'Close menus', function() {
-						this.customizerPage.closeOpenPanel();
-					} );
-				} );
-
-				test.describe( 'Add a new widget', function() {
-					const widgetTitle = dataHelper.getWidgetTitle();
-					const widgetContent = dataHelper.getWidgetContent();
-
-					test.it( 'Expand widgets', function() {
-						this.customizerPage = new CustomizerPage( driver );
-						return this.customizerPage.expandWidgets();
-					} );
-
-					test.it( 'Can add a new sidebar widget as a text widget', function() {
-						return this.customizerPage.addNewSidebarTextWidget( widgetTitle, widgetContent );
-					} );
-
-					test.it( 'Can see the new widget in the preview pane', function() {
-						return this.customizerPage.previewShowsWidget( widgetTitle, widgetContent ).then( ( displayed ) => {
-							return assert( displayed, `The widget with title '${widgetTitle}' and content '${widgetContent}' was not displayed in the preview` );
-						} );
-					} );
-
-					test.it( 'Close widgets', function() {
-						this.customizerPage.closeOpenPanel();
-					} );
-				} );
-
-				test.describe( 'Setting static front page', function() {
-					test.it( 'Expand static front page', function() {
-						this.customizerPage = new CustomizerPage( driver );
-						return this.customizerPage.expandStaticFrontPage();
-					} );
-
-					test.it( 'Can see the front page option', function() {
-						return this.customizerPage.frontPageOptionDisplayed();
-					} );
-
-					test.it( 'Can see the posts page option', function() {
-						return this.customizerPage.postsPageOptionDisplayed();
-					} );
-
-					test.it( 'Close front page section', function() {
-						this.customizerPage.closeOpenSection();
-					} );
-				} );
-
-				test.describe( 'Closing the customizer', function() {
-					test.it( 'Close the customizer', function() {
-						const customizerPage = new CustomizerPage( driver );
-						return customizerPage.close();
 					} );
 				} );
 			} );
