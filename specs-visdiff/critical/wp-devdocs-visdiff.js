51b82e614b07b9b7498a39f56d3065d0381f204f
Scott Stancil <scott.stancil@automattic.com>
2016-08-01 16:39:43 -0400

    Swapped original visdiff spec for cross-browser version [skip ci]

--- specs-visdiff/critical/wp-devdocs-visdiff.js
+++ specs-visdiff/critical/wp-devdocs-visdiff.js
@@ -2,18 +2,16 @@ import test from 'selenium-webdriver/testing';
 import config from 'config';
 import * as driverManager from '../../lib/driver-manager.js';
 import * as driverHelper from '../../lib/driver-helper.js';
-import * as slackNotifier from '../../lib/slack-notifier';
 
 import LoginFlow from '../../lib/flows/login-flow.js';
 
 import DevdocsDesignPage from '../../lib/pages/devdocs-design-page.js';
 
-const mochaVisDiffTimeOut = config.get( 'mochaVisDiffTimeoutMS' );
-const mochaDevDocsTimeOut = config.get( 'mochaDevDocsTimeoutMS' );
-const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
-
 import webdriver from 'selenium-webdriver';
 const by = webdriver.By;
+
+const mochaDevDocsTimeOut = config.get( 'mochaDevDocsTimeoutMS' );
+const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
 
 let driver, screenSize, screenSizeName;
 screenSizeName = driverManager.currentScreenSize();
@@ -22,13 +20,21 @@ let Eyes = require( 'eyes.selenium' ).Eyes;
 let eyes = new Eyes();
 eyes.setApiKey( config.get( 'eyesKey' ) );
 eyes.setForceFullPageScreenshot( true );
+let batchName = '';
 
 if ( process.env.CIRCLE_BUILD_NUM ) {
-	eyes.setBatch( `wp-e2e-tests #${process.env.CIRCLE_BUILD_NUM}`, process.env.CIRCLE_BUILD_NUM );
+	batchName = `wp-e2e-tests #${process.env.CIRCLE_BUILD_NUM}`
+}
+
+if ( config.has( 'sauce' ) && config.get( 'sauce' ) ) {
+	batchName = `Cross Browser Diffs #${process.env.CIRCLE_BUILD_NUM}`
+}
+
+if ( batchName !== '' ) {
+	eyes.setBatch( batchName, process.env.CIRCLE_BUILD_NUM );
 }
 
 test.before( function() {
-	console.log( 'First test.before() in wp-devdocs-visdiff.js' );
 	this.timeout( startBrowserTimeoutMS );
 	driver = driverManager.startBrowser();
 	screenSize = driverManager.getSizeAsObject();
@@ -36,63 +42,84 @@ test.before( function() {
 
 test.describe( 'DevDocs Visual Diff (' + screenSizeName + ')', function() {
 	var devdocsDesignPage;
-	console.log( 'Beginning DevDocs visdiff' );
 	this.timeout( mochaDevDocsTimeOut );
 
 	test.before( function() {
-		var timer;
-
-		console.log( 'Second test.before() in wp-devdocs-visdiff.js, about to open eyes object' );
-		eyes.open( driver, 'WordPress.com', 'DevDocs Design [' + screenSizeName + ']', screenSize ).then( function() {
-			console.log( 'The eyes object is open' );
-		} );
+		eyes.setBaselineName( `cross-browser-devdocs-${screenSizeName}-2` );
+		eyes.setMatchLevel( 'LAYOUT2' );
+		eyes.open( driver, 'WordPress.com', 'DevDocs Cross-Browser [' + screenSizeName + ']', screenSize );
 
 		let loginFlow = new LoginFlow( driver );
-		loginFlow.login().then( function() {
-			console.log( 'Logged in' );
-			timer = setTimeout( function() {
-				console.log( 'DevDocs Page failed to load in time, calling window.stop()' );
-				driver.executeScript( 'window.stop()' );
-			}, 45000 );
-		} );
+		loginFlow.login();
 
 		devdocsDesignPage = new DevdocsDesignPage( driver, true );
-		driver.wait( devdocsDesignPage.displayed(), mochaVisDiffTimeOut ).then( function() {
-			console.log( 'Design Page Loaded' );
-			clearTimeout( timer );
-		} );
+		return devdocsDesignPage.displayed();
 	} );
 
 	test.it( 'Verify UI Components', function() {
-		this.timeout( mochaDevDocsTimeOut );
+		this.timeout( mochaDevDocsTimeOut * 2 );
 		devdocsDesignPage.openUIComponents().then( function() {
-			devdocsDesignPage.getAllDesignElements().then( function( elements ) {
-				// Screenshot each element individually to avoid Applitools page size restrictions
-				for ( const el of elements ) {
-					// Retrieve the name of the element, and the "Compact" button if available
-					el.findElements( by.css( 'h2 a' ) ).then( function( anchors ) {
-						// Two anchors means there's a "Compact" option
-						if ( anchors.length === 2 ) {
-							anchors[0].getInnerHtml().then( function( name ) {
-								driverHelper.eyesScreenshot( driver, eyes, name + ' (Full)', el );
-								anchors[1].click().then( function() {
-									driverHelper.eyesScreenshot( driver, eyes, name + ' (Compact)', el );
-								} );
-							} );
-						} else if ( anchors.length === 1 ) {
-							anchors[0].getInnerHtml().then( function( name ) {
-								driverHelper.eyesScreenshot( driver, eyes, name, el );
-							} );
-						} else if ( anchors.length === 0 ) { // wp-calypso issue #4079, but could recur, so I'm leaving the check
-							el.findElement( by.css( 'h2' ) ).then( function( h2 ) {
-								h2.getInnerHtml().then( function( name ) {
-									driverHelper.eyesScreenshot( driver, eyes, name, el );
-								} );
-							} );
-						} else {
-							throw new Error( 'Unexpected number of "h2 a" tags in Design Element' );
-						}
+			devdocsDesignPage.getAllDesignElementLinks().then( function( hrefs ) {
+				let flow = driver.controlFlow();
+
+				for ( const href of hrefs ) {
+					let title;
+
+					// Open the design element
+					flow.execute( function() {
+						let titleSplit = href.split( '/' );
+						title = titleSplit[ titleSplit.length - 1 ];
+						return driver.get( href );
 					} );
+//					// Scroll back to the top of the page
+//					flow.execute( function() {
+//						return driver.executeScript( 'window.scrollTo( 0, 0 )' );
+//					} );
+//					// Get the title
+//					flow.execute( function() {
+//						return devdocsDesignPage.getCurrentElementTitle().then( function( _title ) {
+//							title = _title;
+//						} );
+//					} );
+					// Hide the masterbar for clean CSS stitching
+					flow.execute( function() {
+						return devdocsDesignPage.hideMasterbar();
+					} );
+					// Take the screenshot
+					flow.execute( function() {
+						return driverHelper.eyesScreenshot( driver, eyes, title, by.id( 'primary' ) );
+					} );
+//					// Scroll back to the top of the page
+//					flow.execute( function() {
+//						return driver.executeScript( 'window.scrollTo( 0, 0 )' );
+//					} );
+					// Check for Compact button
+					flow.execute( function() {
+						return devdocsDesignPage.isCurrentElementCompactable().then( function( compactable ) {
+							if ( compactable ) {
+								return devdocsDesignPage.getCurrentElementCompactButton().then( function( button ) {
+									// Chrome needs a more precise click on the mobile width to avoid overlapping elements
+									if ( global.browserName.toLowerCase() === 'chrome' ) {
+										return driver.actions().mouseMove( button, {x: 3, y: 3} ).click().perform().then( function() {
+											return driverHelper.eyesScreenshot( driver, eyes, title + ' (Compact)', by.id( 'primary' ) );
+										} );
+									}
+
+									return button.click().then( function() {
+										return driverHelper.eyesScreenshot( driver, eyes, title + ' (Compact)', by.id( 'primary' ) );
+									} );
+								} );
+							}
+						} );
+					} );
+//					// Scroll back to the top of the page
+//					flow.execute( function() {
+//						return driver.executeScript( 'window.scrollTo( 0, 0 )' );
+//					} );
+//					// Return to the main list
+//					flow.execute( function() {
+//						return devdocsDesignPage.returnToAllComponents();
+//					} );
 				}
 			} );
 		} );
@@ -100,31 +127,33 @@ test.describe( 'DevDocs Visual Diff (' + screenSizeName + ')', function() {
 
 	test.it( 'Verify Typography', function() {
 		devdocsDesignPage.openTypography().then( function() {
-			driverHelper.eyesScreenshot( driver, eyes, 'DevDocs Design (Typography)' );
+			devdocsDesignPage.hideMasterbar().then( function() {
+				devdocsDesignPage.hideEnvironmentBadge().then( function() {
+					driverHelper.eyesScreenshot( driver, eyes, 'DevDocs Design (Typography)', by.id( 'primary' ) );
+				} );
+			} );
 		} );
 	} );
 
 	test.it( 'Verify App Components', function() {
 		devdocsDesignPage.openAppComponents().then( function() {
-			driverHelper.eyesScreenshot( driver, eyes, 'DevDocs Design (App Components)' );
+			devdocsDesignPage.hideMasterbar().then( function() {
+				devdocsDesignPage.hideEnvironmentBadge().then( function() {
+					driverHelper.eyesScreenshot( driver, eyes, 'DevDocs Design (App Components)', by.id( 'primary' ) );
+				} );
+			} );
 		} );
 	} );
 
 	test.after( function() {
 		try {
 			eyes.close( false ).then( function( testResults ) {
-				let message = '';
-
 				if ( testResults.mismatches ) {
-                                        message = `<!here> Visual diff failed with ${testResults.mismatches} mismatches - ${testResults.url}`;
+					throw new Error( `Visual diff failed with ${testResults.mismatches} mismatches - ${testResults.url}` );
 				} else if ( testResults.missing ) {
-					message = `<!here> Visual diff failed with ${testResults.missing} missing steps out of ${testResults.steps} - ${testResults.url}`;
+					throw new Error( `Visual diff failed with ${testResults.missing} missing steps out of ${testResults.steps} - ${testResults.url}` );
 				} else if ( testResults.isNew ) {
-					message = `<!here> Visual diff marked as failed because it is a new baseline - ${testResults.url}`;
-				}
-
-				if ( message !== '' ) {
-					slackNotifier.warn( message );
+					throw new Error( `Visual diff marked as failed because it is a new baseline - ${testResults.url}` );
 				}
 			} );
 		} finally {
