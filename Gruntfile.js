fc71f170fbf925c4847852dea168f46699457d9b
Scott Stancil <scott.stancil@automattic.com>
2016-08-09 14:59:07 -0400

    Cross-browser diffs now use smaller desktop screensize to allow IE11 [skip ci]

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,9 @@
-'use strict';
-
-module.exports = function (grunt) {
+module.exports = function( grunt ) {
 	// configure tasks
-	grunt.initConfig({
+	grunt.initConfig( {
 		shell: {
 			runTests: {
-				command: function(browserSize, sauceConfig) {
+				command: function( browserSize, sauceConfig ) {
 					return './run.sh -R -c -f -l ' + sauceConfig + ' -s ' + browserSize
 				}
 			}
@@ -13,33 +11,33 @@ module.exports = function (grunt) {
 
 		concurrent: {
 			target: {
-				tasks: [  'run_mobile_osx-chrome', 'run_desktop_osx-chrome', 'run_tablet_osx-chrome',
- 					'run_mobile_osx-firefox', 'run_desktop_osx-firefox', 'run_tablet_osx-firefox',
- 					'run_mobile_osx-safari', 'run_desktop_osx-safari', 'run_tablet_osx-safari' ],
-// 					'run_desktop_win-ie11' ],
+				tasks: [ 'run_mobile_osx-chrome', 'run_desktop_osx-chrome', 'run_tablet_osx-chrome',
+					'run_mobile_osx-firefox', 'run_desktop_osx-firefox', 'run_tablet_osx-firefox',
+					'run_mobile_osx-safari', 'run_desktop_osx-safari', 'run_tablet_osx-safari',
+					'run_desktop_win-ie11' ],
 				options: {
 					limit: 3,
 					logConcurrentOutput: true
 				}
 			}
 		}
-	});
+	} );
 
 	// load tasks
-	grunt.loadNpmTasks('grunt-concurrent');
-	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks( 'grunt-concurrent' );
+	grunt.loadNpmTasks( 'grunt-shell' );
 
 	// register tasks
-	grunt.registerTask('default', ['concurrent:target']);
+	grunt.registerTask( 'default', ['concurrent:target'] );
 
-	grunt.registerTask('run_mobile_osx-chrome', ['shell:runTests:mobile:osx-chrome']);
-	grunt.registerTask('run_desktop_osx-chrome', ['shell:runTests:desktop:osx-chrome']);
-	grunt.registerTask('run_tablet_osx-chrome', ['shell:runTests:tablet:osx-chrome']);
-	grunt.registerTask('run_mobile_osx-firefox', ['shell:runTests:mobile:osx-firefox']);
-	grunt.registerTask('run_desktop_osx-firefox', ['shell:runTests:desktop:osx-firefox']);
-	grunt.registerTask('run_tablet_osx-firefox', ['shell:runTests:tablet:osx-firefox']);
-	grunt.registerTask('run_mobile_osx-safari', ['shell:runTests:mobile:osx-safari']);
-	grunt.registerTask('run_desktop_osx-safari', ['shell:runTests:desktop:osx-safari']);
-	grunt.registerTask('run_tablet_osx-safari', ['shell:runTests:tablet:osx-safari']);
-	grunt.registerTask('run_desktop_win-ie11', ['shell:runTests:desktop:win-ie11']);
+	grunt.registerTask( 'run_mobile_osx-chrome', ['shell:runTests:mobile:osx-chrome'] );
+	grunt.registerTask( 'run_desktop_osx-chrome', ['shell:runTests:desktop-small:osx-chrome'] );
+	grunt.registerTask( 'run_tablet_osx-chrome', ['shell:runTests:tablet:osx-chrome'] );
+	grunt.registerTask( 'run_mobile_osx-firefox', ['shell:runTests:mobile:osx-firefox'] );
+	grunt.registerTask( 'run_desktop_osx-firefox', ['shell:runTests:desktop-small:osx-firefox'] );
+	grunt.registerTask( 'run_tablet_osx-firefox', ['shell:runTests:tablet:osx-firefox'] );
+	grunt.registerTask( 'run_mobile_osx-safari', ['shell:runTests:mobile:osx-safari'] );
+	grunt.registerTask( 'run_desktop_osx-safari', ['shell:runTests:desktop-small:osx-safari'] );
+	grunt.registerTask( 'run_tablet_osx-safari', ['shell:runTests:tablet:osx-safari'] );
+	grunt.registerTask( 'run_desktop_win-ie11', ['shell:runTests:desktop-small:win-ie11'] );
 };
