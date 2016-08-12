7224e463f3a85f647ceaf6f638775c66ba51be4e
Alister Scott <alister.scott@automattic.com>
2016-08-12 15:17:19 +1000

    Completed the first Connect spec

--- specs-jetpack/wp-jetpack-wpcom-connect.js
+++ specs-jetpack/wp-jetpack-wpcom-connect.js
@@ -9,6 +9,9 @@ import WPAdminSidebar from '../lib/pages/wp-admin/wp-admin-sidebar';
 import WPAdminPluginsPage from '../lib/pages/wp-admin/wp-admin-plugins-page';
 import WPAdminJetpackPage from '../lib/pages/wp-admin/wp-admin-jetpack-page';
 import JetpackAuthorizePage from '../lib/pages/jetpack-authorize-page';
+import JetpackPlansPage from '../lib/pages/jetpack-plans-page';
+
+import LoginFlow from '../lib/flows/login-flow';
 
 const mochaTimeOut = config.get( 'mochaTimeoutMS' );
 const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
@@ -42,11 +45,22 @@ test.describe( `Jetpack WordPress.com Connect: '${ screenSize }'`, function() {
 		this.wpAdminPluginsPage.activateJetpack();
 	} );
 
-	test.it( 'Can connect Jetpack to WordPress.com', function() {
+	test.it( 'Can connect Jetpack to WordPress.com and see At A Glance', function() {
 		this.wpAdminSidebar = new WPAdminSidebar( driver );
 		this.wpAdminSidebar.selectJetpack();
 		this.wpAdminJetpackPage = new WPAdminJetpackPage( driver );
 		this.wpAdminJetpackPage.connectWordPressCom();
 		this.jetpackAuthorizePage = new JetpackAuthorizePage( driver );
+		this.jetpackAuthorizePage.chooseSignIn();
+		this.loginFlow = new LoginFlow( driver, 'jetpackConnectUser' );
+		this.loginFlow.loginUsingExistingForm();
+		this.jetpackAuthorizePage = new JetpackAuthorizePage( driver );
+		this.jetpackAuthorizePage.approveConnection();
+		this.jetpackPlansPage = new JetpackPlansPage( driver );
+		this.jetpackPlansPage.chooseFreePlan();
+		this.wpAdminJetpackPage = new WPAdminJetpackPage( driver );
+		this.wpAdminJetpackPage.atAGlanceDisplayed().then( ( atAGlanceDisplayed ) => {
+			assert( atAGlanceDisplayed, 'The WP Admin Jetpack \'At a Glance\' page is not displayed after running Jetpack Connect' );
+		} );
 	} );
 } );
