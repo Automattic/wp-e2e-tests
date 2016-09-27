6a44e8b30838e3b144c72d60a549e985fff2f186
Scott Stancil <scott.stancil@automattic.com>
2016-09-27 12:14:19 -0400

    Removed visdiffs from run-wrapper

--- run-wrapper.sh
+++ run-wrapper.sh
@@ -22,9 +22,6 @@ if [ "$RUN_SPECIFIED" == "true" ]; then
   TESTARGS=$RUN_ARGS
 elif [ "$CIRCLE_BRANCH" == "master" ]; then
   TESTARGS="-R -p" # Parallel execution, implies -g -s mobile,desktop,tablet
-  if [ "$RUN_VISDIFF" == "true" ] && [ "$DEPLOY_USER" != "" ]; then
-    TESTARGS+=" -v critical"
-  fi
 fi
 
 if [ "$liveBranches" == "true" ]; then
