ba84c86f2f60d2f660606f087ce258ba28d09275
Scott Stancil <hoverduck@users.noreply.github.com>
2016-09-30 14:27:30 -0400

    Override Sauce Labs /etc/hosts file to prevent loading external images (#278)

---
+++ fix-saucelabs-etc-hosts.bat
@@ -0,0 +1,4 @@
+@echo off
+echo. >> C:\Windows\System32\drivers\etc\hosts
+echo 127.0.0.1 lorempixel.com >> C:\Windows\System32\drivers\etc\hosts
+echo 127.0.0.1 placekitten.com >> C:\Windows\System32\drivers\etc\hosts
