e925ad34fef1c75eb9a81fe98f4fc6b658d40448
Julian de Bhal <julian.debhal@automattic.com>
2017-03-28 23:22:12 -0400

    Add redirect tests for mag16

---
+++ specs-i18n/logged-out-redirect-fr-spec.js
@@ -0,0 +1,33 @@
+/**
+ * External Dependencies
+ */
+import test from 'selenium-webdriver/testing';
+import config from 'config';
+
+/**
+ * Internal Dependencies
+ */
+import * as driverManager from '../lib/driver-manager.js';
+
+import WPHomePage from '../lib/pages/wp-home-page';
+
+const mochaTimeOut = config.get( 'mochaTimeoutMS' );
+const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
+
+var driver;
+
+test.before( function() {
+	this.timeout( startBrowserTimeoutMS );
+} );
+
+test.describe( 'wordpress.com is redirected to the correct url for fr', function() {
+	this.timeout( mochaTimeOut );
+
+	test.it( 'should redirect to the correct url for fr.wordpress.com', function() {
+		driver = driverManager.startBrowser( { acceptLanguage: 'fr' } );
+
+		// No culture here implies 'en'
+		this.wpHomePage = new WPHomePage( driver, { visit: true } );
+		this.wpHomePage.checkURL( 'fr' );
+	} );
+} );
