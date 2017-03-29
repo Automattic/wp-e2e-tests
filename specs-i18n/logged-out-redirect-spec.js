28018f4aa66fce3c45f2ad6deee45a7c5863c048
Julian de Bhal <julian.debhal@automattic.com>
2017-03-29 14:10:55 -0400

    rework logged-out-redirect-*-spec.js to use LOCALE_TEST environment variable

--- specs-i18n/logged-out-redirect-spec.js
+++ specs-i18n/logged-out-redirect-spec.js
@@ -14,20 +14,23 @@ import WPHomePage from '../lib/pages/wp-home-page';
 const mochaTimeOut = config.get( 'mochaTimeoutMS' );
 const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
 
+// call run.sh with -I to feed in the mag16
+const testLocale = process.env.LOCALE_TEST || 'en';
+
 var driver;
 
 test.before( function() {
 	this.timeout( startBrowserTimeoutMS );
 } );
 
-test.describe( 'wordpress.com is redirected to the correct url for ja', function() {
+test.describe( `logged out homepage redirect test (${ testLocale })`, function() {
 	this.timeout( mochaTimeOut );
 
-	test.it( 'should redirect to the correct url for ja.wordpress.com', function() {
-		driver = driverManager.startBrowser( { acceptLanguage: 'ja' } );
+	test.it( `should redirect to the correct url for wordpress.com (${ testLocale })`, function() {
+		driver = driverManager.startBrowser( { acceptLanguage: testLocale } );
 
 		// No culture here implies 'en'
 		this.wpHomePage = new WPHomePage( driver, { visit: true } );
-		this.wpHomePage.checkURL( 'ja' );
+		this.wpHomePage.checkURL( testLocale );
 	} );
 } );
