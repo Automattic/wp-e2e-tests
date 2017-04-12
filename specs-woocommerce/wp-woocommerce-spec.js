733de5ca3789bf9a37396e654b983268d7169637
Alister Scott <alister.scott@automattic.com>
2017-04-12 10:07:05 +1000

    Add WooCommerce store pages

--- specs-woocommerce/wp-woocommerce-spec.js
+++ specs-woocommerce/wp-woocommerce-spec.js
@@ -4,7 +4,9 @@ import test from 'selenium-webdriver/testing';
 import config from 'config';
 import * as driverManager from '../lib/driver-manager';
 
-import StatsPage from '../lib/pages/stats-page';
+import StorePage from '../lib/pages/woocommerce/store-page';
+import AddProductPage from '../lib/pages/woocommerce/add-product-page';
+import StoreStatsPage from '../lib/pages/woocommerce/store-stats-page';
 
 import LoginFlow from '../lib/flows/login-flow';
 
@@ -27,9 +29,30 @@ test.describe( `WooCommerce on Calypso: '${ screenSize }'`, function() {
 		driverManager.clearCookiesAndDeleteLocalStorage( driver );
 	} );
 
-	test.it( 'Can see stats page for a WooCommerce site', function() {
-		let loginFlow = new LoginFlow( driver, 'wooCommerceUser' );
-		loginFlow.loginAndSelectMySite();
-		this.statsPage = new StatsPage( driver );
+	// Login as WooCommerce store user
+	test.before( function() {
+		this.loginFlow = new LoginFlow( driver, 'wooCommerceUser' );
+		this.loginFlow.loginAndSelectMySite();
+	} );
+
+	test.it( 'Can see store placeholder page when visiting /store', function() {
+		this.storePage = new StorePage( driver, true );
+		this.storePage.displayed().then( ( shown ) => {
+			assert( shown, 'Could not see the WooCommerce store page after visting /store' );
+		} );
+	} );
+
+	test.it( 'Can see the add product placeholder page when visiting /store/products/add', function() {
+		this.addProductPage = new AddProductPage( driver, true );
+		this.addProductPage.displayed().then( ( shown ) => {
+			assert( shown, 'Could not see the WooCommerce add product page after visting /store/products/add' );
+		} );
+	} );
+
+	test.it( 'Can see the add store stats placeholder page when visiting /store/stats', function() {
+		this.storeStatsPage = new StoreStatsPage( driver, true );
+		this.storeStatsPage.displayed().then( ( shown ) => {
+			assert( shown, 'Could not see the WooCommerce add product page after visting /store/stats' );
+		} );
 	} );
 } );
