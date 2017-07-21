e1520379e1653204762e8d3781fc053d7df9fdb1
Eric Binnion <ericbinnion@gmail.com>
2017-07-21 00:06:19 -0500

    Tests now working through first page

--- specs-jetpack-calypso/wp-jpo-bluehost-spec.js
+++ specs-jetpack-calypso/wp-jpo-bluehost-spec.js
@@ -3,12 +3,16 @@ import config from 'config';
 // import assert from 'assert';
 
 import * as driverManager from '../lib/driver-manager';
-// import * as dataHelper from '../lib/data-helper';
+import { allowRetries } from '../lib/driver-helper';
 
 import BluehostLoginPage from '../lib/pages/bluehost/bluehost-login-page';
 import BluehostInstallWordPressPage from '../lib/pages/bluehost/bluehost-install-wordpress-page';
 import BluehostInstallWordPressDetailsPage from '../lib/pages/bluehost/bluehost-install-wordpress-details-page';
 import BluehostInstallingwordPressPage from '../lib/pages/bluehost/bluehost-installing-wordpress-page';
+
+import BluehostJPOFirstStepPage from '../lib/pages/bluehost/bluehost-jpo-first-step-page';
+
+import WPAdminLogonPage from '../lib/pages/wp-admin/wp-admin-logon-page';
 
 const mochaTimeOut = config.get( 'mochaTimeoutMS' );
 const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
@@ -49,6 +53,41 @@ test.describe( 'Can step through JPO flow on Bluehost', function() {
 		test.it( 'can finish WordPress installation', () => {
 			this.bluehostInstallingWordPressPage = new BluehostInstallingwordPressPage( driver );
 			return this.bluehostInstallingWordPressPage.finishesInstall();
+		} );
+	} );
+
+	test.describe( 'Can login to newly provisioned site', () => {
+		test.it( 'login succeeds', () => {
+			const url = `${config.get( 'bluehostTestDomain' )}/wp-login.php`,
+				args = { visit: true, forceStandardLogon: true },
+				userLogin = config.get( 'bluehostWPInstallUsername' ),
+				userPass = config.get( 'bluehostWPInstallPassword' );
+
+			return allowRetries( driver, () => {
+				this.loginPage = new WPAdminLogonPage( driver, url, args );
+				return this.loginPage.logonStandard( userLogin, userPass )
+			}, 3 );
+		} );
+	} );
+
+	test.describe( 'Can step through JPO flow', () => {
+		test.describe( 'can interact with first step page', () => {
+			test.it( 'can select business type site', () => {
+				this.bluehostJPOFirstStepPage = new BluehostJPOFirstStepPage( driver, true );
+				return this.bluehostJPOFirstStepPage.selectBusinessType();
+			} );
+
+			test.it( 'can reset JPO', () => {
+				return this.bluehostJPOFirstStepPage.resetJPO();
+			} );
+
+			test.it( 'can select personal type site', () => {
+				return this.bluehostJPOFirstStepPage.selectPersonalType();
+			} );
+
+			test.it( 'can submit site title and description form', () => {
+				return this.bluehostJPOFirstStepPage.submitSiteTitleAndDescriptionForm();
+			} );
 		} );
 	} );
 } );
