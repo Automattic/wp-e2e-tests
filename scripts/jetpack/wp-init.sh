cb378f4ff3c610616eee91724e31a596e525e174
Scott Stancil <scott.stancil@automattic.com>
2017-07-25 13:28:27 -0400

    Updated init scripts [skip ci]

--- scripts/jetpack/wp-init.sh
+++ scripts/jetpack/wp-init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Initialize WordPress with default info and unique URL
 node /wp-tunnel-init.js&
+
+# Install Jetpack master branch from git
+cd /var/www/html/wp-content/plugins
+git clone --depth 1 https://github.com/Automattic/jetpack.git
 
 exec apache2-foreground
--- scripts/jetpack/wp-tunnel-init.js
+++ scripts/jetpack/wp-tunnel-init.js
@@ -1,19 +1,23 @@
 var WP = require( 'wp-cli' );
 var localtunnel = require( 'localtunnel' );
 
-localtunnel( 80, function( err, tunnel ) {
+var ltPrefix = '';
+
+var t = localtunnel( 80, function( err, tunnel ) {
 	if ( err ) {
 		console.log( 'UH OH! - Failed to establish localtunnel - ' + err );
+		return;
 	}
 
-	console.log( 'Tunnel: ' + tunnel.url );
+	console.log( 'Tunnel Established: ' + tunnel.url );
+	ltPrefix = tunnel.url.replace( /https:\/\//, '' ).split( '.' )[0];
 
 	WP.discover( { path: '/var/www/html' }, function( wp ) {
 		wp.core.install( {
 			url: tunnel.url,
-			title: 'Docker FTW',
+			title: 'e2e-Jetpack',
 			admin_user: 'wordpress',
-			admin_password: 'Abcde123',
+			admin_password: 'password', // TODO - Make this better
 			admin_email: 'user@example.com'
 		}, function( wperr, info ) {
 			console.log( 'Install complete! - ' + info );
@@ -21,3 +25,27 @@ localtunnel( 80, function( err, tunnel ) {
 	} );
 } );
 
+var ltCloseHandler = function() {
+	console.log( 'LocalTunnel CLOSED' );
+};
+
+var ltErrorHandler = function( receivedError ) {
+	console.log( `LocalTunnel ERROR, attempting reconnect - ${receivedError}` );
+	t.close();
+
+	t = localtunnel( 80, { subdomain: ltPrefix }, function( err, tunnel ) {
+		if ( err ) {
+			console.log( 'UH OH! - Failed to establish localtunnel - ' + err );
+			return;
+		}
+
+		console.log( 'Tunnel: ' + tunnel.url );
+		ltPrefix = tunnel.url.replace( /https:\/\//, '' ).split( '.' )[0];
+	} );
+
+	t.on( 'error', ltErrorHandler );
+	t.on( 'close', ltCloseHandler );
+};
+
+t.on( 'error', ltErrorHandler );
+t.on( 'close', ltCloseHandler );
