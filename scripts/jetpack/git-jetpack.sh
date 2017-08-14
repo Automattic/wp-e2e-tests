bed9a2200fb9927ecc9bbe24cf8ab43fc19d933b
Scott Stancil <scott.stancil@automattic.com>
2017-08-14 15:12:51 -0400

    Added wait to Jetpack install [skip ci]

---
+++ scripts/jetpack/git-jetpack.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+export APP_NAME=$1
+if [ "$APP_NAME" == "" ]; then
+  echo "Please supply app directory name!"
+  exit 1
+elif [ ! -d apps/$APP_NAME ]; then
+  # Wait up to 30 seconds for app to be created
+  echo "App directory apps/$APP_NAME does not exist, waiting..."
+  sleep 10
+  if [ ! -d apps/$APP_NAME ]; then
+    echo "App directory apps/$APP_NAME does not exist, waiting..."
+    sleep 10
+    if [ ! -d apps/$APP_NAME ]; then
+      echo "App directory apps/$APP_NAME does not exist, waiting..."
+      sleep 10
+      if [ ! -d apps/$APP_NAME ]; then
+        echo "App directory apps/$APP_NAME does not exist after 30s, quitting!"
+        exit 1
+      fi
+    fi
+  fi
+fi
+echo "App directory apps/$APP_NAME found!  Installing Jetpack..."
+
+cd apps/$APP_NAME/public/wp-content/plugins
+git clone --depth 1 https://github.com/Automattic/jetpack.git
+cd jetpack
+yarn build
