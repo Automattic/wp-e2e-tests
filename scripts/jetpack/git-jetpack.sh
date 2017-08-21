491d04bc6623d6464356d3c4b8361383cdb1b117
Scott Stancil <scott.stancil@automattic.com>
2017-08-21 16:01:30 -0400

    Updated path to check in git-jetpack.sh

--- scripts/jetpack/git-jetpack.sh
+++ scripts/jetpack/git-jetpack.sh
@@ -3,18 +3,18 @@ export APP_NAME=$1
 if [ "$APP_NAME" == "" ]; then
   echo "Please supply app directory name!"
   exit 1
-elif [ ! -d apps/$APP_NAME ]; then
+elif [ ! -d apps/$APP_NAME/public/wp-content/plugins ]; then
   # Wait up to 30 seconds for app to be created
-  echo "App directory apps/$APP_NAME does not exist, waiting..."
+  echo "App directory apps/$APP_NAME/public/wp-content/plugins does not exist, waiting..."
   sleep 10
-  if [ ! -d apps/$APP_NAME ]; then
-    echo "App directory apps/$APP_NAME does not exist, waiting..."
+  if [ ! -d apps/$APP_NAME/public/wp-content/plugins ]; then
+    echo "App directory apps/$APP_NAME/public/wp-content/plugins does not exist, waiting..."
     sleep 10
     if [ ! -d apps/$APP_NAME ]; then
-      echo "App directory apps/$APP_NAME does not exist, waiting..."
+      echo "App directory apps/$APP_NAME/public/wp-content/plugins does not exist, waiting..."
       sleep 10
       if [ ! -d apps/$APP_NAME ]; then
-        echo "App directory apps/$APP_NAME does not exist after 30s, quitting!"
+        echo "App directory apps/$APP_NAME/public/wp-content/plugins does not exist after 30s, quitting!"
         exit 1
       fi
     fi
@@ -23,7 +23,6 @@ fi
 echo "App directory apps/$APP_NAME found!  Installing Jetpack..."
 
 # Refresh jetpack in home directory and copy it into the site
-mkdir -p apps/$APP_NAME/public/wp-content/plugins
 cp -a jetpack apps/$APP_NAME/public/wp-content/plugins
 cd apps/$APP_NAME/public/wp-content/plugins/jetpack
 git checkout master && git pull
