5fb36ed3b6bc9c3c339e247fdc9f747e4ac70474
Alister Scott <alister.scott@gmail.com>
2017-08-24 13:55:40 +1000

    Remove Jetpack from payment button as this requires a plan

--- specs/wp-post-editor-spec.js
+++ specs/wp-post-editor-spec.js
@@ -928,7 +928,7 @@ test.describe( `[${host}] Editor: Posts (${screenSize})`, function() {
 		} );
 	} );
 
-	test.describe( 'Insert a payment button: @parallel @jetpack', function() {
+	test.describe( 'Insert a payment button: @parallel', function() {
 		this.bailSuite( true );
 
 		test.it( 'Delete Cookies and Local Storage', function() {
