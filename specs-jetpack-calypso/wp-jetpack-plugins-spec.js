0233e8ab4cc7d31bb1ca83959b595d669ee2ab4c
Scott Stancil <hoverduck@users.noreply.github.com>
2017-09-05 16:00:58 -0400

    Updated selectors and logic for Jetpack Plugins page (#706)

--- specs-jetpack-calypso/wp-jetpack-plugins-spec.js
+++ specs-jetpack-calypso/wp-jetpack-plugins-spec.js
@@ -9,8 +9,6 @@ import PluginsPage from '../lib/pages/plugins-page';
 import PluginsBrowserPage from '../lib/pages/plugins-browser-page';
 
 import PluginDetailsPage from '../lib/pages/plugin-details-page';
-import SidebarComponent from '../lib/components/sidebar-component';
-import NavbarComponent from '../lib/components/navbar-component';
 import LoginFlow from '../lib/flows/login-flow';
 
 const mochaTimeOut = config.get( 'mochaTimeoutMS' );
@@ -35,7 +33,7 @@ test.describe( `[${host}] Jetpack Sites on Calypso - Existing Plugins: (${screen
 
 	test.before( function() {
 		let loginFlow = new LoginFlow( driver, 'jetpackUser' + host );
-		loginFlow.loginAndSelectPlugins();
+		loginFlow.loginAndSelectManagePlugins();
 	} );
 
 	test.describe( 'Can activate Hello Dolly', function() {
@@ -84,10 +82,6 @@ test.describe( `[${host}] Jetpack Sites on Calypso - Searching Plugins: (${scree
 		test.it( 'Open the plugins browser and find WP Job Manager by searching for Automattic', function() {
 			const pluginVendor = 'WP Job Manager';
 			const pluginTitle = 'WP Job Manager';
-			this.navbarComponent = new NavbarComponent( driver );
-			this.navbarComponent.clickMySites();
-			this.sidebarComponent = new SidebarComponent( driver );
-			this.sidebarComponent.selectAddPlugin();
 			this.pluginsBrowserPage = new PluginsBrowserPage( driver );
 			this.pluginsBrowserPage.searchForPlugin( pluginVendor );
 			this.pluginsBrowserPage.pluginTitledShown( pluginTitle, pluginVendor ).then( ( pluginDisplayed ) => {
