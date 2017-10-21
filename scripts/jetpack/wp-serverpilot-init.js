39b3b99751094e6dc2a588a774c8357fc3c50d4e
oskosk <oskosk@gmail.com>
2017-10-21 11:55:56 -0300

    Wait in a blocking fashion after until app is ready when initializing it

--- scripts/jetpack/wp-serverpilot-init.js
+++ scripts/jetpack/wp-serverpilot-init.js
@@ -15,6 +15,7 @@ const username = userConfig.jetpackUserCI[0];
 const password = userConfig.jetpackUserCI[1];
 
 const serverPrefix = process.env.CIRCLE_SHA1.substr( 0, 20 );
+const actionWaitTimeout = 2000;
 
 const serverOptions = {
 	name: `wordpress-${serverPrefix}`,
@@ -33,6 +34,34 @@ sp.createApp( serverOptions, function( err, data ) {
 		console.log( err );
 		throw err;
 	}
-
-	console.log( `Site created - http://${serverPrefix}.wp-e2e-tests.pw - ID ${data.data.id}` );
+	waitForServerPilotAction( data.actionid, function( err ) {
+		if ( err !== null ) {
+			console.log( err );
+			throw err;
+		}
+		console.log( `Site created - http://${serverPrefix}.wp-e2e-tests.pw - ID ${data.data.id}` );
+	} );
 } );
+
+/**
+ * Waits for ServerPilot to finish an action
+ * @param  {String}   actionId The ServerPilot action Id as returned by the create app endpoint
+ * @param  {Function} cb       What to do after the action finishes or errors
+ */
+function waitForServerPilotAction( actionId, cb ) {
+	setTimeout( function() {
+		sp.getActionStatus( actionId, function( err, response ) {
+			if ( err ) {
+				return cb( err );
+			}
+			if ( response.data.status === 'error' ) {
+				return cb( new Error( 'ServerPilot app creation has completed but there were errors.' ) );
+			}
+			// If ServerPilot is still provisioning the app, recur
+			if ( response.data.status === 'open' ) {
+				return waitForServerPilotAction( actionId, cb );
+			}
+			cb();
+		} );
+	}, actionWaitTimeout );
+}
