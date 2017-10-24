75f2f821ba018e84cdda1dbb06bddee565909a77
Steve Seear <steve.seear@gmail.com>
2017-10-24 21:46:26 +0100

    Log into wpcom and click 'add new site button'

--- specs-jetpack-calypso/wp-jetpack-connect-spec.js
+++ specs-jetpack-calypso/wp-jetpack-connect-spec.js
@@ -7,6 +7,8 @@ import * as driverManager from '../lib/driver-manager';
 import * as dataHelper from '../lib/data-helper';
 
 import WporgCreatorPage from '../lib/pages/wporg-creator-page';
+import LoginFlow from '../lib/flows/login-flow';
+import SidebarComponent from '../lib/components/sidebar-component';
 
 const mochaTimeOut = config.get( 'mochaTimeoutMS' );
 const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
@@ -40,8 +42,14 @@ test.describe( `Jetpack Connect: (${ screenSize }) @jetpack`, function() {
 			} );
 		} );
 
-		test.it( 'Has URL', () => {
-			console.log( 'url', this.url );
+		test.it( 'Can add new site', () => {
+			const loginFlow = new LoginFlow( driver );
+			loginFlow.loginAndSelectMySite();
+
+			const sidebarComponent = new SidebarComponent( driver );
+			sidebarComponent.addNewSite( driver );
+
+			// some A/B test in play here?
 		} );
 	} );
 } );
