1fee374e304396533419d953f7c2dce43b8e1c0d
Alister Scott <alister.scott@gmail.com>
2018-01-31 14:13:14 +1000

    Clean up ensuredSaved

--- specs/wp-post-editor-spec.js
+++ specs/wp-post-editor-spec.js
@@ -390,6 +390,7 @@ test.describe( `[${host}] Editor: Posts (${screenSize})`, function() {
 				} );
 				postEditorSidebarComponent.closeStatusSection();
 				let editorPage = new EditorPage( driver );
+				editorPage.waitForPage();
 				postEditorToolbarComponent = new PostEditorToolbarComponent( driver );
 				postEditorToolbarComponent.ensureSaved( { clickSave: true } );
 				return postEditorToolbarComponent.clickPublishPost();
