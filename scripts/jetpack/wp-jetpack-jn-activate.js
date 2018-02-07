2c26c25e04f715d04e70513996c10fd85d16fcb1
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2018-02-07 23:45:54 +0700

    make sure disconnected sites removed

--- scripts/jetpack/wp-jetpack-jn-activate.js
+++ scripts/jetpack/wp-jetpack-jn-activate.js
@@ -2,7 +2,6 @@ import test from 'selenium-webdriver/testing';
 import config from 'config';
 
 const fs = require( 'fs' );
-const debug = require( 'debug' )( 'e2e-tests' );
 
 import JetpackConnectFlow from '../../lib/flows/jetpack-connect-flow.js';
 
@@ -17,12 +16,29 @@ const host = dataHelper.getJetpackHost();
 
 let driver;
 
+// Write url and site credentials into file for further use
+function writeJNCredentials( url, username, password ) {
+	const fileContents = `${url} ${username} ${password}`;
+	return fs.mkdir( './temp', err => {
+		if ( err && err.code !== 'EEXIST' ) {
+			return console.log( err );
+		}
+
+		fs.writeFile( './temp/jn-credentials.txt', fileContents, fileErr => {
+			if ( fileErr ) {
+				return console.log( fileErr );
+			}
+			console.log( 'The jn-credentials file was saved!' );
+		} );
+	} );
+}
+
 test.before( function() {
 	this.timeout( startBrowserTimeoutMS );
 	driver = driverManager.startBrowser();
 } );
 
-test.describe( `[${host}] Jetpack Connection: (${screenSize}) @jetpack`, function() {
+test.describe( `[${host}] Jurassic Ninja Connection: (${screenSize}) @jetpack`, function() {
 	this.timeout( mochaTimeOut );
 	this.bailSuite( true );
 
@@ -30,36 +46,14 @@ test.describe( `[${host}] Jetpack Connection: (${screenSize}) @jetpack`, functio
 		return driverManager.clearCookiesAndDeleteLocalStorage( driver );
 	} );
 
-	test.it( 'Can log into WordPress.com', () => {
-		if ( host === 'JN' ) {
-			debug( 'starting host: %s', host );
-
-			const jnFlow = new JetpackConnectFlow( driver, 'jetpackUserJN' );
-			// jnFlow.removeSites().then( () => {
-			// 	return jnFlow.connect2();
-			// } ).then( () => {
-			return jnFlow.connect2().then( () => {
-				const wpDashboard = new WPAdminDashboardPage( driver, jnFlow.url );
-				return wpDashboard.logout();
-			} ).then( () => {
-				// Write url and site credentials into file for further use
-				const fileContents = `${jnFlow.url} demo ${jnFlow.password}`;
-				return fs.mkdir( './temp', err => {
-					if ( err && err.code !== 'EEXIST' ) {
-						return console.log( err );
-					}
-
-					fs.writeFile( './temp/jn-credentials.txt',
-						fileContents,
-						fileErr => {
-							if ( fileErr ) {
-								return console.log( fileErr );
-							}
-							console.log( 'The file was saved!' );
-						} );
-				} );
-			} );
-		}
-		return false;
+	test.it( 'Can remove diconnected sites and connect new one ', () => {
+		const jnFlow = new JetpackConnectFlow( driver, 'jetpackUserJN' );
+		jnFlow.removeSites();
+		return jnFlow.connectFromWPAdmin()
+		.then( () => {
+			const wpDashboard = new WPAdminDashboardPage( driver, jnFlow.url );
+			return wpDashboard.logout();
+		} )
+		.then( () => writeJNCredentials( jnFlow.url, 'demo', jnFlow.password ) );
 	} );
 } );
