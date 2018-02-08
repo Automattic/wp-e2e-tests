dd3823047dbb1b00b57f2b9d3cbd7b309c4e0458
Scott Stancil <scott.stancil@automattic.com>
2018-02-08 13:11:06 -0500

    Hopefully cache is now fixed...

--- run.sh
+++ run.sh
@@ -23,10 +23,6 @@ if [ "$CI" == "true" ]; then
 
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   nvm install
-
-  # Weird issue on CircleCI not properly building this package on the first try...
-  rm -rf node_modules/fibers
-  npm install
 fi
 
 # Function to join arrays into a string
