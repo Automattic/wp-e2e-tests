fc925c85c394508d2c5f642dde9801d2633583a1
Scott Stancil <scott.stancil@automattic.com>
2018-02-09 10:13:11 -0500

    Husky

--- specs/wp-manage-domains-spec.js
+++ specs/wp-manage-domains-spec.js
@@ -2,7 +2,7 @@ import assert from "assert";
 import test from "selenium-webdriver/testing";
 
 import config from "config";
-import * as driverManager from '../lib/driver-manager.js';
+import * as driverManager from "../lib/driver-manager.js";
 import * as dataHelper from '../lib/data-helper.js';
 
 import DomainsPage from '../lib/pages/domains-page.js';
