95f4d1eff0b47d8e2ac6f586e0173329ef5ab35e
Scott Stancil <scott.stancil@automattic.com>
2018-02-09 10:25:09 -0500

    Final commit with no errors

--- specs/wp-manage-domains-spec.js
+++ specs/wp-manage-domains-spec.js
@@ -1,9 +1,9 @@
-import assert from "assert";
-import test from "selenium-webdriver/testing";
+import assert from 'assert';
+import test from 'selenium-webdriver/testing';
 
-import config from "config";
-import * as driverManager from "../lib/driver-manager.js";
-import * as dataHelper from "../lib/data-helper.js";
+import config from 'config';
+import * as driverManager from '../lib/driver-manager.js';
+import * as dataHelper from '../lib/data-helper.js';
 
 import DomainsPage from '../lib/pages/domains-page.js';
 import CheckOutPage from '../lib/pages/signup/checkout-page.js';
