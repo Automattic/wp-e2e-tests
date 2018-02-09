7483095eddf9b628d0359af5e66ccb9ff5f9744b
Scott Stancil <scott.stancil@automattic.com>
2018-02-09 09:53:35 -0500

    Attempt commit with linting errors

--- specs/wp-manage-domains-spec.js
+++ specs/wp-manage-domains-spec.js
@@ -1,5 +1,5 @@
 import assert from 'assert';
-import test from 'selenium-webdriver/testing';
+import test from "selenium-webdriver/testing";
 
 import config from 'config';
 import * as driverManager from '../lib/driver-manager.js';
