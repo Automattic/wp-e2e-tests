1a73318e2af0d55c552c0fb86ef3b574c6c9efc7
Alister Scott <alister.scott@gmail.com>
2018-02-13 18:20:33 +1000

    Update trash post to expect reader (no refresh on editor page)

--- specs/wp-post-editor-spec.js
+++ specs/wp-post-editor-spec.js
@@ -851,10 +851,10 @@ test.describe( `[${host}] Editor: Posts (${screenSize})`, function() {
 				return postEditorSidebarComponent.trashPost();
 			} );
 
-			test.it( 'Can then see the Posts page (new)', function() {
-				const postsPage = new PostsPage( driver );
-				return postsPage.displayed().then( ( displayed ) => {
-					return assert.equal( displayed, true, 'The posts page is not displayed' );
+			test.it( 'Can then see the Reader page', function() {
+				const readerPage = new ReaderPage( driver );
+				return readerPage.displayed().then( ( displayed ) => {
+					return assert.equal( displayed, true, 'The reader page is not displayed' );
 				} );
 			} );
 		} );
