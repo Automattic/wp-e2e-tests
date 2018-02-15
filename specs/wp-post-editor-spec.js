6e8c2d8bcd862bd5165fd9d6caa269fde763ffe1
Scott Stancil <hoverduck@users.noreply.github.com>
2018-02-15 12:18:12 -0500

    Re-enabling Publish a New Post test
    * wp-calypso/22258 was fixed, re-enabling the tests
    
    * Fix linting error that somehow snuck in

--- specs/wp-post-editor-spec.js
+++ specs/wp-post-editor-spec.js
@@ -22,7 +22,6 @@ import * as driverManager from '../lib/driver-manager';
 import * as mediaHelper from '../lib/media-helper';
 import * as dataHelper from '../lib/data-helper';
 import * as eyesHelper from '../lib/eyes-helper.js';
-import * as SlackNotifier from '../lib/slack-notifier';
 
 const mochaTimeOut = config.get( 'mochaTimeoutMS' );
 const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
@@ -865,10 +864,9 @@ test.describe( `[${host}] Editor: Posts (${screenSize})`, function() {
 
 		test.it( 'Delete Cookies and Local Storage', function() {
 			driverManager.clearCookiesAndDeleteLocalStorage( driver );
-			return SlackNotifier.warn( 'Not running the edit a post test because of https://github.com/Automattic/wp-calypso/issues/22258', { suppressDuplicateMessages: true } );
 		} );
 
-		test.xdescribe( 'Publish a New Post', function() {
+		test.describe( 'Publish a New Post', function() {
 			const originalBlogPostTitle = dataHelper.randomPhrase();
 			const updatedBlogPostTitle = dataHelper.randomPhrase();
 			const blogPostQuote = 'Science is organised knowledge. Wisdom is organised life..\n~ Immanuel Kant\n';
