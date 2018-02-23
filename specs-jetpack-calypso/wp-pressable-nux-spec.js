8848db70856b1dffc5652c2800df1d9aaf7903b8
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2018-02-23 20:38:22 +0800

    Now use actual NUX flow

--- specs-jetpack-calypso/wp-pressable-nux-spec.js
+++ specs-jetpack-calypso/wp-pressable-nux-spec.js
@@ -2,24 +2,17 @@ import test from 'selenium-webdriver/testing';
 import config from 'config';
 import assert from 'assert';
 
-import WPAdminJetpackPage from '../lib/pages/wp-admin/wp-admin-jetpack-page';
 import PressableLogonPage from '../lib/pages/pressable/pressable-logon-page';
 import PressableSitesPage from '../lib/pages/pressable/pressable-sites-page';
 import PressableApprovePage from '../lib/pages/pressable-approve-page';
 import PressableSiteSettingsPage from '../lib/pages/pressable/pressable-site-settings-page';
 import JetpackAuthorizePage from '../lib/pages/jetpack-authorize-page';
-import ReaderPage from '../lib/pages/reader-page';
-import StatsPage from '../lib/pages/stats-page';
-import ActivityPage from '../lib/pages/stats/activity-page';
 import WPAdminDashboardPage from '../lib/pages/wp-admin/wp-admin-dashboard-page';
-
-import WPAdminSidebar from '../lib/pages/wp-admin/wp-admin-sidebar';
-import NavbarComponent from '../lib/components/navbar-component';
-import SidebarComponent from '../lib/components/sidebar-component';
 
 import * as driverManager from '../lib/driver-manager';
 import * as dataHelper from '../lib/data-helper';
 import LoginPage from '../lib/pages/login-page';
+import PressableNUXFlow from '../lib/flows/pressable-nux-flow';
 
 const mochaTimeOut = config.get( 'mochaTimeoutMS' );
 const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
@@ -79,43 +72,13 @@ test.describe( `[${host}] Pressable NUX: (${screenSize}) @jetpack`, function() {
 			return this.jetpackAuthorizePage.approveConnection();
 		} );
 
-		test.it( 'Can navigate to the Jetpack dashboard', function() {
-			this.wpAdminSidebar = new WPAdminSidebar( driver );
-			return this.wpAdminSidebar.selectJetpack();
-		} );
-
-		test.it( 'Can activate recommended features', function() {
-			this.jetpackDashboard = new WPAdminJetpackPage( driver );
-			return this.jetpackDashboard.activateRecommendedFeatures();
-		} );
-
-		test.it( 'Can open recently created site stats', function() {
-			const siteURL = `${this.siteName}.mystagingwebsite.com`;
-			// const siteURL = 'e2eflowtesting1519293477608988.mystagingwebsite.com';
-			//
-			this.readerPage = new ReaderPage( driver, true );
-
-			this.navbarComponent = new NavbarComponent( driver );
-			return this.navbarComponent.clickMySites()
-			.then( () => {
-				let sidebarComponent = new SidebarComponent( driver );
-				return sidebarComponent.selectSiteSwitcher()
-				.then( () => sidebarComponent.searchForSite( siteURL ) )
-				.then( () => sidebarComponent.selectStats() );
-			} );
-		} );
-
-		test.it( 'Can add site credentials', function() {
-			this.statsPage = new StatsPage( driver );
-			return this.statsPage.openActivity()
-			.then( () => {
-				this.activityPage = new ActivityPage( driver );
-				return this.activityPage.addSiteCredentials();
-			} );
+		test.it( 'Can proceed with Pressable NUX flow', function() {
+			this.nuxFlow = new PressableNUXFlow( driver );
+			return this.nuxFlow.addSiteCredentials();
 		} );
 
 		test.it( 'Can navigate to the Jetpack dashboard', function() {
-			this.wpDashboardPage = new WPAdminDashboardPage( driver, this.siteName + '.mystagingwebsite.com' );
+			this.wpDashboardPage = new WPAdminDashboardPage( driver );
 			return this.wpDashboardPage.isJITMessageDisplayed( 'rewind' ).then( ( shown ) => {
 				assert( !shown, 'Rewind JITM is still visible' );
 			} );
