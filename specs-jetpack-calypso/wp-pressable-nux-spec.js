fb1aff54cd0f2f70f144d456b14aa8e91c9b2edd
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2018-03-05 21:42:35 +0700

    try check backup status on Activity page

--- specs-jetpack-calypso/wp-pressable-nux-spec.js
+++ specs-jetpack-calypso/wp-pressable-nux-spec.js
@@ -1,18 +1,23 @@
 import test from 'selenium-webdriver/testing';
 import config from 'config';
-import assert from 'assert';
+// import assert from 'assert';
 
 import PressableLogonPage from '../lib/pages/pressable/pressable-logon-page';
 import PressableSitesPage from '../lib/pages/pressable/pressable-sites-page';
 import PressableApprovePage from '../lib/pages/pressable/pressable-approve-page';
 import PressableSiteSettingsPage from '../lib/pages/pressable/pressable-site-settings-page';
 import JetpackAuthorizePage from '../lib/pages/jetpack-authorize-page';
-import WPAdminDashboardPage from '../lib/pages/wp-admin/wp-admin-dashboard-page';
+// import WPAdminDashboardPage from '../lib/pages/wp-admin/wp-admin-dashboard-page';
 
 import * as driverManager from '../lib/driver-manager';
 import * as dataHelper from '../lib/data-helper';
 import LoginPage from '../lib/pages/login-page';
 import PressableNUXFlow from '../lib/flows/pressable-nux-flow';
+import ReaderPage from '../lib/pages/reader-page';
+import SidebarComponent from '../lib/components/sidebar-component';
+import StatsPage from '../lib/pages/stats-page';
+import ActivityPage from '../lib/pages/stats/activity-page';
+import NavbarComponent from '../lib/components/navbar-component';
 
 const mochaTimeOut = config.get( 'mochaTimeoutMS' );
 const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
@@ -79,12 +84,38 @@ test.describe( `[${host}] Pressable NUX: (${screenSize}) @jetpack`, function() {
 		return this.nuxFlow.addSiteCredentials();
 	} );
 
-	test.it( 'Can see expected content in the Jetpack dashboard', function() {
-		this.wpDashboardPage = new WPAdminDashboardPage( driver );
-		return this.wpDashboardPage.isJITMessageDisplayed( 'rewind' ).then( ( shown ) => {
-			assert( !shown, 'Rewind JITM is still visible' );
+	test.it( 'Can open Rewind activity page', function() {
+		this.readerPage = new ReaderPage( driver, true );
+		let navbarComponent = new NavbarComponent( driver );
+		return navbarComponent.clickMySites()
+		.then( () => {
+			this.sidebarComponent = new SidebarComponent( driver );
+			return this.sidebarComponent.selectSiteSwitcher();
+		} )
+		.then( () => this.sidebarComponent.searchForSite( this.siteName ) )
+		.then( () => this.sidebarComponent.selectStats() )
+		.then( () => {
+			this.statsPage = new StatsPage( driver );
+			return this.statsPage.openActivity();
 		} );
 	} );
+
+	test.it( 'Can wait until Rewind backup is completed', function() {
+		this.activityPage = new ActivityPage( driver );
+		return this.activityPage.expandDayCard()
+		.then( () => this.activityPage.waitUntilBackupCompleted() );
+	} );
+	// test.it( 'Can check if the initial backup is underway', function() {
+	// 	this.activityPage = new ActivityPage( driver );
+	// 	assert( this.activityPage.isBackupUnderway(), 'Initial backup is not initiated' );
+	// } );
+
+	// test.it( 'Can see expected content in the Jetpack dashboard', function() {
+	// 	this.wpDashboardPage = new WPAdminDashboardPage( driver );
+	// 	return this.wpDashboardPage.isJITMessageDisplayed( 'rewind' ).then( ( shown ) => {
+	// 		assert( !shown, 'Rewind JITM is still visible' );
+	// 	} );
+	// } );
 
 	test.after( function() {
 		this.pressableSitesPage = new PressableSitesPage( driver, true );
