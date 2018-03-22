f3aa1e9e4b63ed9e392f98bc7512833cdab6938f
Alister Scott <alister.scott@gmail.com>
2018-03-22 10:12:16 +1000

    Clean up the map domain spec

--- specs/wp-manage-domains-map-spec.js
+++ specs/wp-manage-domains-map-spec.js
@@ -1,4 +1,3 @@
-import assert from 'assert';
 import test from 'selenium-webdriver/testing';
 
 import config from 'config';
@@ -24,104 +23,76 @@ const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
 const screenSize = driverManager.currentScreenSize();
 const host = dataHelper.getJetpackHost();
 
-var driver;
+let driver;
 
 test.before( function() {
 	this.timeout( startBrowserTimeoutMS );
 	driver = driverManager.startBrowser();
 } );
 
-test.describe( `[${host}] Managing Domain Mapping: (${screenSize}) @parallel`, function() {
-	this.bailSuite( true );
+test.describe( `[${host}] Managing Domain Mapping: (${screenSize})`, function() {
 	this.timeout( mochaTimeOut );
 
-	test.describe( 'Map a domain to an existing site', () => {
+	test.describe( 'Map a domain to an existing site @parallel', function() {
 		this.bailSuite( true );
 
 		const blogName = 'myawesomedomain.com';
 
 		test.before( function() {
-			driverManager.clearCookiesAndDeleteLocalStorage( driver );
+			return driverManager.ensureNotLoggedIn( driver );
 		} );
 
-		test.it( 'Log In and Select Domains', () => {
-			const loginFlow = new LoginFlow( driver );
-			loginFlow.loginAndSelectDomains();
+		test.it( 'Log In and Select Domains', function() {
+			return new LoginFlow( driver ).loginAndSelectDomains();
 		} );
 
-		test.describe( 'Can find domain mapping section and enter a domain to map', function() {
-			test.it( 'Can choose add a domain', () => {
-				const domainsPage = new DomainsPage( driver );
-				driver.getCurrentUrl().then( ( urlDisplayed ) => {
-					domainsPage.setABTestControlGroupsInLocalStorage( urlDisplayed );
-				} );
-				return domainsPage.clickAddDomain();
+		test.it( 'Can see the Domains page and choose add a domain', function() {
+			const domainsPage = new DomainsPage( driver );
+			driver.getCurrentUrl().then( ( urlDisplayed ) => {
+				domainsPage.setABTestControlGroupsInLocalStorage( urlDisplayed );
 			} );
+			return domainsPage.clickAddDomain();
+		} );
 
-			test.it( 'Can see the domain search component', () => {
-				const findADomainComponent = new FindADomainComponent( driver );
-				findADomainComponent.waitForResults();
-			} );
+		test.it( 'Can see the domain search component', function() {
+			return new FindADomainComponent( driver ).waitForResults();
+		} );
 
-			test.it( 'Can select to use an existing domain', () => {
-				const findADomainComponent = new FindADomainComponent( driver );
-				findADomainComponent.selectUseOwnDomain();
-			} );
+		test.it( 'Can select to use an existing domain', function() {
+			return new FindADomainComponent( driver ).selectUseOwnDomain();
+		} );
 
-			test.it( 'Can see use my own domain page', () => {
-				const myOwnDomainPage = new MyOwnDomainPage( driver );
-				myOwnDomainPage.displayed().then( ( displayed ) => {
-					assert.equal( displayed, true, 'Could not see use my own domain page' );
-				} );
-			} );
+		test.it( 'Can see use my own domain page', function() {
+			return new MyOwnDomainPage( driver ).displayed();
+		} );
 
-			test.it( 'Can select to manually connect existing domain component', () => {
-				const mapADomainComponent = new MapADomainComponent( driver );
-				mapADomainComponent.selectManuallyConnectExistingDomain();
-			} );
+		test.it( 'Can select to manually connect existing domain component', function() {
+			return new MapADomainComponent( driver ).selectManuallyConnectExistingDomain();
+		} );
 
-			test.it( 'Can see enter a domain component', () => {
-				const mapADomainPage = new MapADomainPage( driver );
-				mapADomainPage.displayed().then( ( displayed ) => {
-					assert.equal( displayed, true, 'Could not see map a domain page' );
-				} );
-			} );
+		test.it( 'Can see enter a domain component', function() {
+			return new MapADomainPage( driver ).displayed();
+		} );
 
-			test.it( 'Can enter the domain name', () => {
-				const enterADomainComponent = new EnterADomainComponent( driver );
-				enterADomainComponent.enterADomain( blogName );
-			} );
+		test.it( 'Can enter the domain name', function() {
+			return new EnterADomainComponent( driver ).enterADomain( blogName );
+		} );
 
-			test.it( 'Can add domain to the cart', () => {
-				const enterADomainComponent = new EnterADomainComponent( driver );
-				enterADomainComponent.clickonAddButtonToAddDomainToTheCart();
-			} );
+		test.it( 'Can add domain to the cart', function() {
+			return new EnterADomainComponent( driver ).clickonAddButtonToAddDomainToTheCart();
+		} );
 
-			test.describe( 'Can pay for domain mapping', function() {
-				test.it( 'Can see checkout page', () => {
-					const mapADomainCheckoutPage = new MapADomainCheckoutPage( driver );
-					mapADomainCheckoutPage.displayed().then( ( displayed ) => {
-						assert.equal( displayed, true, 'Could not see the secure payment component' );
-					} );
-				} );
+		test.it( 'Can see checkout page', function() {
+			return new MapADomainCheckoutPage( driver ).displayed();
+		} );
 
-				// Remove all items from basket for clean up
-				test.it( 'Can empty cart for next test', () => {
-					this.readerPage = new ReaderPage( driver, true );
-
-					this.navbarComponent = new NavbarComponent( driver );
-					this.navbarComponent.clickMySites();
-
-					this.statsPage = new StatsPage( driver, true );
-
-					this.sideBarComponent = new SidebarComponent( driver );
-					this.sideBarComponent.selectDomains();
-
-					this.domainsPage = new DomainsPage( driver );
-					this.shoppingCartWidgetComponent = new ShoppingCartWidgetComponent( driver );
-					return this.shoppingCartWidgetComponent.empty();
-				} );
-			} );
+		test.after( function() { // Empty the cart
+			new ReaderPage( driver, true ).displayed();
+			new NavbarComponent( driver ).clickMySites();
+			new StatsPage( driver, true ).displayed();
+			new SidebarComponent( driver ).selectDomains();
+			new DomainsPage( driver ).displayed();
+			return new ShoppingCartWidgetComponent( driver ).empty();
 		} );
 	} );
 } );
