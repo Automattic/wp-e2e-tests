00cb669c60b7206e404c2fa490ae5fb69df41edc
stojdza <ivanstojadinov@gmail.com>
2018-05-07 23:56:53 +0200

    add async calls to wp-pressable-nux-spec.js

--- specs-jetpack-calypso/wp-pressable-nux-spec.js
+++ specs-jetpack-calypso/wp-pressable-nux-spec.js
@@ -23,12 +23,12 @@ const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
 const screenSize = driverManager.currentScreenSize();
 const host = dataHelper.getJetpackHost();
 
-var driver;
+let driver;
 
 if ( host === 'PRESSABLE' ) {
-	test.before( function() {
+	test.before( async function() {
 		this.timeout( startBrowserTimeoutMS );
-		driver = driverManager.startBrowser();
+		driver = await driverManager.startBrowser();
 	} );
 
 	test.describe( `[${ host }] Pressable NUX: (${ screenSize })`, function() {
@@ -37,52 +37,52 @@ if ( host === 'PRESSABLE' ) {
 		test.describe( 'Disconnect expired sites: @parallel @jetpack', function() {
 			this.bailSuite( true );
 
-			test.before( function() {
-				return driverManager.ensureNotLoggedIn( driver );
+			test.before( async function() {
+				return await driverManager.ensureNotLoggedIn( driver );
 			} );
 
-			test.it( 'Can disconnect any expired sites', () => {
-				return new JetpackConnectFlow( driver ).removeSites();
+			test.it( 'Can disconnect any expired sites', async function() {
+				return await new JetpackConnectFlow( driver ).removeSites();
 			} );
 		} );
 
 		test.describe( 'Connect via Pressable @parallel @jetpack', function() {
 			this.bailSuite( true );
 
-			test.before( function() {
-				return driverManager.ensureNotLoggedIn( driver );
+			test.before( async function() {
+				return await driverManager.ensureNotLoggedIn( driver );
 			} );
 
-			test.it( 'Can log into WordPress.com', function() {
-				return new LoginFlow( driver, 'jetpackUser' + host ).login();
+			test.it( 'Can log into WordPress.com', async function() {
+				return await new LoginFlow( driver, 'jetpackUser' + host ).login();
 			} );
 
-			test.it( 'Can log into Pressable', function() {
-				return new PressableLogonPage( driver, true ).loginWithWP();
+			test.it( 'Can log into Pressable', async function() {
+				return await new PressableLogonPage( driver, true ).loginWithWP();
 			} );
 
-			test.it( 'Can approve login with WordPress', function() {
-				return new PressableApprovePage( driver ).approve();
+			test.it( 'Can approve login with WordPress', async function() {
+				return await new PressableApprovePage( driver ).approve();
 			} );
 
-			test.it( 'Can create new site', function() {
+			test.it( 'Can create new site', async function() {
 				this.siteName = dataHelper.getNewBlogName();
 				this.pressableSitesPage = new PressableSitesPage( driver );
-				return this.pressableSitesPage.addNewSite( this.siteName );
+				return await this.pressableSitesPage.addNewSite( this.siteName );
 			} );
 
-			test.it( 'Can go to site settings', function() {
-				return this.pressableSitesPage.gotoSettings( this.siteName );
+			test.it( 'Can go to site settings', async function() {
+				return await this.pressableSitesPage.gotoSettings( this.siteName );
 			} );
 
-			test.it( 'Can proceed to Jetpack activation', function() {
+			test.it( 'Can proceed to Jetpack activation', async function() {
 				const siteSettings = new PressableSiteSettingsPage( driver );
-				siteSettings.waitForJetpackPremium();
-				return siteSettings.activateJetpackPremium();
+				await siteSettings.waitForJetpackPremium();
+				return await siteSettings.activateJetpackPremium();
 			} );
 
-			test.it( 'Can approve connection on the authorization page', function() {
-				return new JetpackAuthorizePage( driver ).approveConnection();
+			test.it( 'Can approve connection on the authorization page', async function() {
+				return await new JetpackAuthorizePage( driver ).approveConnection();
 			} );
 
 			test.it(
@@ -92,18 +92,18 @@ if ( host === 'PRESSABLE' ) {
 				}
 			);
 
-			test.it( 'Can proceed with Pressable NUX flow', function() {
-				return new PressableNUXFlow( driver ).addSiteCredentials();
+			test.it( 'Can proceed with Pressable NUX flow', async function() {
+				return await new PressableNUXFlow( driver ).addSiteCredentials();
 			} );
 
-			test.it( 'Can open Rewind activity page', function() {
-				new ReaderPage( driver, true ).displayed();
-				new NavbarComponent( driver ).clickMySites();
+			test.it( 'Can open Rewind activity page', async function() {
+				await new ReaderPage( driver, true ).displayed();
+				await new NavbarComponent( driver ).clickMySites();
 				const sidebarComponent = new SidebarComponent( driver );
-				sidebarComponent.selectSiteSwitcher();
-				sidebarComponent.searchForSite( this.siteName );
-				sidebarComponent.selectStats();
-				return new StatsPage( driver ).openActivity();
+				await sidebarComponent.selectSiteSwitcher();
+				await sidebarComponent.searchForSite( this.siteName );
+				await sidebarComponent.selectStats();
+				return await new StatsPage( driver ).openActivity();
 			} );
 
 			// Disabled due to to longer time is required to make a backup.
@@ -112,8 +112,8 @@ if ( host === 'PRESSABLE' ) {
 			// 	return activityPage.waitUntilBackupCompleted();
 			// } );
 
-			test.after( function() {
-				return new PressableSitesPage( driver, true ).deleteFirstSite();
+			test.after( async function() {
+				return await new PressableSitesPage( driver, true ).deleteFirstSite();
 			} );
 		} );
 	} );
