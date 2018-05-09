ca3227140552ae401b4a2e05fb574b048432f6fa
Alister Scott <alister.scott@gmail.com>
2018-05-09 14:52:17 +1000

    Revert "Fix IE11 temporarily (#1197)" (#1198)
    This reverts commit 407b0d0b49db67eb8d9803bbec9929d1ac1a62f9.

--- scripts/fix-saucelabs-etc-hosts.bat
+++ scripts/fix-saucelabs-etc-hosts.bat
@@ -3,4 +3,3 @@ echo. >> C:\Windows\System32\drivers\etc\hosts
 echo 127.0.0.1 lorempixel.com >> C:\Windows\System32\drivers\etc\hosts
 echo 127.0.0.1 placekitten.com >> C:\Windows\System32\drivers\etc\hosts
 echo 127.0.0.1 pixel.wp.com >> C:\Windows\System32\drivers\etc\hosts
-echo 162.222.75.33 ondemand.saucelabs.com >> C:\Windows\System32\drivers\etc\hosts
--- scripts/fix-saucelabs-etc-hosts.sh
+++ scripts/fix-saucelabs-etc-hosts.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "127.0.0.1 lorempixel.com" >> /etc/hosts
 echo "127.0.0.1 placekitten.com" >> /etc/hosts
-echo "162.222.75.33 ondemand.saucelabs.com" >> /etc/hosts
