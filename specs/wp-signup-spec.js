c93617a575c92b4ed7f66f31277c15b27ce251ce
Alister Scott <alister.scott@gmail.com>
2018-06-04 09:12:16 +1000

    Revert "Update email client with latest mailosaur api"

--- specs/wp-signup-spec.js
+++ specs/wp-signup-spec.js
@@ -159,9 +159,15 @@ test.describe( `[${ host }] Sign Up  (${ screenSize }, ${ locale })`, function()
 
 			test.it( 'Can see email containing magic link', async function() {
 				const emailClient = new EmailClient( signupInboxId );
-				let email = await emailClient.waitForEmailByRecipient( emailAddress );
-				if ( email.subject.includes( 'WordPress.com' ) ) {
-					return ( magicLoginLink = email.html.links[ 0 ].href );
+				const validator = emails =>
+					emails.find( email => email.subject.includes( 'WordPress.com' ) );
+				let emails = await emailClient.pollEmailsByRecipient( emailAddress, validator );
+				//Disabled due to a/b test on activation email. See https://github.com/Automattic/wp-e2e-tests/issues/819
+				//assert.equal( emails.length, 2, 'The number of newly registered emails is not equal to 2 (activation and magic link)' );
+				for ( let email of emails ) {
+					if ( email.subject.includes( 'WordPress.com' ) ) {
+						return ( magicLoginLink = email.html.links[ 0 ].href );
+					}
 				}
 				return assert(
 					magicLoginLink !== undefined,
