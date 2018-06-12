f09af003f7b8e0f978be2f95543e456bbdd330c9
Alister Scott <alister.scott@gmail.com>
2018-06-12 16:29:00 +1000

    oops

--- specs/wp-post-editor-spec.js
+++ specs/wp-post-editor-spec.js
@@ -368,7 +368,7 @@ test.describe( `[${ host }] Editor: Posts (${ screenSize })`, function() {
 		} );
 	} );
 
-	test.describe.only( 'Basic Public Post @canary @parallel @jetpack', function() {
+	test.describe( 'Basic Public Post @canary @parallel @jetpack', function() {
 		this.bailSuite( true );
 
 		test.it( 'Delete Cookies and Local Storage', async function() {
