fd4ca6f359ddd5b903166c51f6d1813c5ad9bab3
Abidh Ahmed <7570415+abidhahmed@users.noreply.github.com>
2018-07-26 12:41:49 +0530

    Update Jetpack smoke test to use concurrently npm module (#1369)

--- specs-jetpack/runner.sh
+++ specs-jetpack/runner.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 export JETPACKHOST=PRESSABLE
 export TARGET=JETPACK
-array=( 'goDaddyJetpackUser' 'asoJetpackUser' 'goDaddyJetpackUserSub'
-'bluehostJetpackUser' 'bluehostJetpackUserSub' 'siteGroundJetpackUser')
 
-for i in "${array[@]}"
-do
-  export JETPACKUSER="$i"
-	eval "./node_modules/.bin/mocha ./specs-jetpack/connect-disconnect-spec.js &"
-done
+eval "./node_modules/.bin/concurrently 'JETPACKUSER=goDaddyJetpackUser ./node_modules/.bin/mocha ./specs-jetpack/connect-disconnect-spec.js' 'JETPACKUSER=asoJetpackUser ./node_modules/.bin/mocha ./specs-jetpack/connect-disconnect-spec.js' 'JETPACKUSER=goDaddyJetpackUserSub ./node_modules/.bin/mocha ./specs-jetpack/connect-disconnect-spec.js' 'JETPACKUSER=bluehostJetpackUser ./node_modules/.bin/mocha ./specs-jetpack/connect-disconnect-spec.js' 'JETPACKUSER=bluehostJetpackUserSub ./node_modules/.bin/mocha ./specs-jetpack/connect-disconnect-spec.js' 'JETPACKUSER=siteGroundJetpackUser ./node_modules/.bin/mocha ./specs-jetpack/connect-disconnect-spec.js'"
