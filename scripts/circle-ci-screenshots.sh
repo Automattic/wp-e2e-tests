82ac763824afcfdd6ade2cd8089f548ac69d5e2a
Brent Sessions <brent.sessions@automattic.com>
2018-07-27 13:41:20 -0600

    trying script changes

--- scripts/circle-ci-screenshots.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt install -y libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev ffmpeg xvfb
-nohup Xvfb -ac :99 -screen 0 1440x1000x16 +extension RANDR > /dev/null 2>&1 &
-
-export DISPLAY=:99
-export TEST_VIDEO='true'
