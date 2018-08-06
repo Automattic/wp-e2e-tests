9a7405843f000c42eac26abe5a2fecfeaf92ffdc
Javon Davis <javonldavis14@gmail.com>
2018-08-06 00:18:45 -0500

    * Using custom data type to determine onboarding step * Update skip step to accept a step number and use custom data type to wait for element before skipping
    Fix #1196

--- specs-jetpack-calypso/wp-jetpack-onboarding-spec.js
+++ specs-jetpack-calypso/wp-jetpack-onboarding-spec.js
@@ -55,16 +55,16 @@ describe( `Jetpack Onboarding: (${ screenSize })`, function() {
 		} );
 
 		step( 'Can navigate to onboarding flow', async function() {
-			return await driver.get( this.jnFlow.url + onboardingUrlExt );
+			let domain = 'http://calypso.localhost:3000/jetpack/start/';
+			return await driver.get( domain + this.jnFlow.url.substring( 8 ) );
+			// return await driver.get( this.jnFlow.url + onboardingUrlExt );
 		} );
 
 		step( 'Can skip all steps', async function() {
 			const wizardNavigationComponent = await WizardNavigationComponent.Expect( driver );
-			await wizardNavigationComponent.skipStep();
-			await wizardNavigationComponent.skipStep();
-			await wizardNavigationComponent.skipStep();
-			await wizardNavigationComponent.skipStep();
-			await wizardNavigationComponent.skipStep();
+			for ( let step_number = 1; step_number < 6; step_number++ ) {
+				await wizardNavigationComponent.skipStep( step_number );
+			}
 			const summaryPage = await SummaryPage.Expect( driver );
 			let toDoCount = await summaryPage.countToDoSteps();
 			assert.strictEqual( toDoCount, 4, 'Expected and actual steps are not equal.' );
@@ -157,7 +157,9 @@ describe( `Jetpack Onboarding: (${ screenSize })`, function() {
 		} );
 
 		step( 'Can navigate to onboarding flow', async function() {
-			return await driver.get( this.jnFlow.url + onboardingUrlExt );
+			let domain = 'http://calypso.localhost:3000/jetpack/start/';
+			return await driver.get( domain + this.jnFlow.url.substring( 8 ) );
+			// return await driver.get( this.jnFlow.url + onboardingUrlExt );
 		} );
 
 		step( 'Can fill out site title and tagline', async function() {
@@ -179,7 +181,7 @@ describe( `Jetpack Onboarding: (${ screenSize })`, function() {
 
 		step( 'Can skip add a contact form', async function() {
 			const wizardNavigationComponent = await WizardNavigationComponent.Expect( driver );
-			return await wizardNavigationComponent.skipStep();
+			return await wizardNavigationComponent.skipStep( 4 );
 		} );
 
 		step( 'Can select add a business address', async function() {
@@ -279,7 +281,9 @@ describe( `Jetpack Onboarding: (${ screenSize })`, function() {
 		} );
 
 		step( 'Can navigate to onboarding flow', async function() {
-			return await driver.get( this.jnFlow.url + onboardingUrlExt );
+			let domain = 'http://calypso.localhost:3000/jetpack/start/';
+			return await driver.get( domain + this.jnFlow.url.substring( 8 ) );
+			// return await driver.get( this.jnFlow.url + onboardingUrlExt );
 		} );
 
 		step( 'Can fill out site title and tagline', async function() {
@@ -301,12 +305,12 @@ describe( `Jetpack Onboarding: (${ screenSize })`, function() {
 
 		step( 'Can skip add a contact form', async function() {
 			const wizardNavigationComponent = await WizardNavigationComponent.Expect( driver );
-			return await wizardNavigationComponent.skipStep();
+			return await wizardNavigationComponent.skipStep( 4 );
 		} );
 
 		step( 'Can skip add a business address', async function() {
 			const wizardNavigationComponent = await WizardNavigationComponent.Expect( driver );
-			return await wizardNavigationComponent.skipStep();
+			return await wizardNavigationComponent.skipStep( 5 );
 		} );
 
 		step( 'Can make business an online store', async function() {
@@ -391,7 +395,9 @@ describe( `Jetpack Onboarding: (${ screenSize })`, function() {
 		} );
 
 		step( 'Can navigate to onboarding flow', async function() {
-			return await driver.get( this.jnFlow.url + onboardingUrlExt );
+			let domain = 'http://calypso.localhost:3000/jetpack/start/';
+			return await driver.get( domain + this.jnFlow.url.substring( 8 ) );
+			// return await driver.get( this.jnFlow.url + onboardingUrlExt );
 		} );
 
 		step( 'Can fill out site title and tagline', async function() {
