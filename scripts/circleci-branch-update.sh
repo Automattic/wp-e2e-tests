ef07456ed5310fc487e5dd63411f132d04508f50
Brent Sessions <brent.sessions@automattic.com>
2018-08-15 12:08:51 +0200

    fixing curl

--- scripts/circleci-branch-update.sh
+++ scripts/circleci-branch-update.sh
@@ -4,13 +4,14 @@ version=$(<.nvmrc)
 version=${version:1}
 config=( $(jq -r '.circleCIToken' ./config/local-${NODE_ENV}.json) )
 token=${config[0]}
+echo ${token}
 
-curl -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/gh/automattic/wp-e2e-tests-for-branches/envvar?circle-token=${token}
-curl -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/gh/automattic/wp-e2e-canary-for-branches/envvar?circle-token=${token}
-curl -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/gh/automattic/wp-e2e-tests-canary/envvar?circle-token=${token}
-curl -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/gh/automattic/wp-e2e-tests-ie11/envvar?circle-token=${token}
-curl -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/gh/automattic/wp-e2e-tests-jetpack/envvar?circle-token=${token}
-curl -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/gh/automattic/wp-e2e-tests-jetpack-be/envvar?circle-token=${token}
-curl -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/gh/automattic/wp-e2e-tests-woocommerce/envvar?circle-token=${token}
-curl -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/gh/automattic/wp-e2e-tests-visdiff/envvar?circle-token=${token}
-curl -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/gh/automattic/wp-e2e-tests-i18n/envvar?circle-token=${token}
+curl --write-out %{http_code} --silent --output /dev/null -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/github/automattic/wp-e2e-tests-for-branches/envvar?circle-token=${token}
+curl --write-out %{http_code} --silent --output /dev/null -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/github/automattic/wp-e2e-canary-for-branches/envvar?circle-token=${token}
+curl --write-out %{http_code} --silent --output /dev/null -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/github/automattic/wp-e2e-tests-canary/envvar?circle-token=${token}
+curl --write-out %{http_code} --silent --output /dev/null -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/github/automattic/wp-e2e-tests-ie11/envvar?circle-token=${token}
+curl --write-out %{http_code} --silent --output /dev/null -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/github/automattic/wp-e2e-tests-jetpack/envvar?circle-token=${token}
+curl --write-out %{http_code} --silent --output /dev/null -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/github/automattic/wp-e2e-tests-jetpack-be/envvar?circle-token=${token}
+curl --write-out %{http_code} --silent --output /dev/null -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/github/automattic/wp-e2e-tests-woocommerce/envvar?circle-token=${token}
+curl --write-out %{http_code} --silent --output /dev/null -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/github/automattic/wp-e2e-tests-visdiff/envvar?circle-token=${token}
+curl --write-out %{http_code} --silent --output /dev/null -X POST --header "Content-Type: application/json" -d '{"name":"NODE_VERSION", "value":"${version}"}' https://circleci.com/api/v1.1/project/github/automattic/wp-e2e-tests-i18n/envvar?circle-token=${token}
