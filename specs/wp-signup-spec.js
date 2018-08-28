85e969d805874626838b816ba8059556b3c0ad69
Alister Scott <alister.scott@gmail.com>
2018-08-28 08:06:02 +0200

    Fix the signup tests to include a canary (#1434)

--- specs/wp-signup-spec.js
+++ specs/wp-signup-spec.js
@@ -1204,7 +1204,7 @@ describe( `[${ host }] Sign Up  (${ screenSize }, ${ locale })`, function() {
 		} );
 	} );
 
-	describe( 'Basic sign up for a free site @parallel @email @ie11canary', function() {
+	describe( 'Basic sign up for a free site @parallel @email @ie11canary @canary', function() {
 		const blogName = dataHelper.getNewBlogName();
 
 		before( async function() {
