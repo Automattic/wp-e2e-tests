75dfa83d41ff28670e6e028b78c134071db6096b
Alister Scott <alister.scott@gmail.com>
2018-09-17 15:45:13 +1000

    Try free canary for signup

--- specs/wp-signup-spec.js
+++ specs/wp-signup-spec.js
@@ -337,7 +337,7 @@ describe( `[${ host }] Sign Up  (${ screenSize }, ${ locale })`, function() {
 		} );
 	} );
 
-	describe( 'Sign up for a site on a premium paid plan through main flow in USD currency @parallel @canary', function() {
+	describe( 'Sign up for a site on a premium paid plan through main flow in USD currency @parallel', function() {
 		const blogName = dataHelper.getNewBlogName();
 		const expectedBlogAddresses = dataHelper.getExpectedFreeAddresses( blogName );
 		const emailAddress = dataHelper.getEmailAddress( blogName, signupInboxId );
@@ -1253,7 +1253,7 @@ describe( `[${ host }] Sign Up  (${ screenSize }, ${ locale })`, function() {
 		} );
 	} );
 
-	describe( 'Basic sign up for a free site @parallel @email @ie11canary', function() {
+	describe( 'Basic sign up for a free site @parallel @email @ie11canary @canary', function() {
 		const blogName = dataHelper.getNewBlogName();
 
 		before( async function() {
