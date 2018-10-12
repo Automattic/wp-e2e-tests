51289b006c70fb6e7bd30c435371c971eaf0b8a9
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2018-10-12 15:59:06 +0300

    Run Gutenpack tests against JN site which includes PR changes

--- run.sh
+++ run.sh
@@ -86,7 +86,7 @@ while getopts ":a:RpS:b:B:s:gjWCJH:wzyl:cm:fiIUvxu:h" opt; do
       ;;
     S)
       export LIVEBRANCHES="true"
-      NODE_CONFIG_ARGS+=("\"liveBranch\":\"true\",\"calypsoBaseURL\":\"https://hash-$OPTARG.calypso.live\"")
+      NODE_CONFIG_ARGS+=("\"liveBranch\":\"true\",\"calypsoBaseURL\":\"https://hash-$OPTARG.calypso.live\",\"branchName\":\"$BRANCHNAME\"")
       continue
       ;;
     b)
