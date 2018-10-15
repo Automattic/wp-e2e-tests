dcad1da05feec4b4efc638ed87d691133f896327
Brent Sessions <brent.sessions@automattic.com>
2018-10-15 11:33:06 -0600

    Updating script

--- scripts/circleci-branch-update.sh
+++ scripts/circleci-branch-update.sh
@@ -27,7 +27,9 @@ head-changed-file () {
 update-node () {
     version=$(<.nvmrc)
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-    source ~/.bashrc &&
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
     nvm install $version &&
     nvm alias default $version
 }
