5a13fa8ef11a23c59fd9374208118aedd3b67181
Alister Scott <alister.scott@automattic.com>
2018-11-09 11:40:14 +0000

    Another one

--- specs/wp-signup-spec.js
+++ specs/wp-signup-spec.js
@@ -302,19 +302,10 @@ describe( `[${ host }] Sign Up  (${ screenSize }, ${ locale })`, function() {
 		step(
 			'Can search for a blog name, can see and select a free WordPress.com blog address in results',
 			async function() {
-				const findADomainComponent = await FindADomainComponent.Expect( driver );
-				await findADomainComponent.searchForBlogNameAndWaitForResults( blogName );
-				await findADomainComponent.checkAndRetryForFreeBlogAddresses(
-					expectedBlogAddresses,
-					blogName
+				return await new SignUpFlow( driver ).selectFreeWordPressDotComAddresss(
+					blogName,
+					expectedBlogAddresses
 				);
-				let actualAddress = await findADomainComponent.freeBlogAddress();
-				assert(
-					expectedBlogAddresses.indexOf( actualAddress ) > -1,
-					`The displayed free blog address: '${ actualAddress }' was not the expected addresses: '${ expectedBlogAddresses }'`
-				);
-
-				return await findADomainComponent.selectFreeAddress();
 			}
 		);
 
