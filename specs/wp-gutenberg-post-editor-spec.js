468f9f984d6abf361900c813d520a4064dfbe36a
Alister Scott <alister.scott@gmail.com>
2018-11-22 12:27:25 +1000

    Add Private Post Spec

--- specs/wp-gutenberg-post-editor-spec.js
+++ specs/wp-gutenberg-post-editor-spec.js
@@ -16,7 +16,6 @@ import PaypalCheckoutPage from '../lib/pages/external/paypal-checkout-page';
 import SidebarComponent from '../lib/components/sidebar-component.js';
 import NavBarComponent from '../lib/components/nav-bar-component.js';
 import GutenbergPostPreviewComponent from '../lib/gutenberg/gutenberg-post-preview-component';
-import PostEditorSidebarComponent from '../lib/components/post-editor-sidebar-component.js';
 import PostEditorToolbarComponent from '../lib/components/post-editor-toolbar-component';
 import GutenbergEditorComponent from '../lib/gutenberg/gutenberg-editor-component';
 import WPAdminPostsPage from '../lib/pages/wp-admin/wp-admin-posts-page';
@@ -368,129 +367,130 @@ describe( `[${ host }] Gutenberg Editor: Posts (${ screenSize })`, function() {
 		} );
 	} );
 
-	xdescribe( 'Private Posts: @parallel', function() {
+	describe( 'Private Posts: @parallel', function() {
 		describe( 'Publish a Private Post', function() {
 			const blogPostTitle = dataHelper.randomPhrase();
 			const blogPostQuote =
 				'If you’re not prepared to be wrong; you’ll never come up with anything original.\n— Sir Ken Robinson\n';
 
+			before( async function() {
+				if ( driverManager.currentScreenSize() === 'mobile' ) {
+					await SlackNotifier.warn(
+						'Gutenberg private post spec currently not supported on mobile due to Gutenberg bug',
+						{ suppressDuplicateMessages: true }
+					);
+					return this.skip();
+				}
+			} );
+
 			step( 'Can log in', async function() {
-				let loginFlow = new LoginFlow( driver, 'gutenbergSimpleSiteUser' );
-				await loginFlow.loginAndStartNewPost();
+				this.loginFlow = new LoginFlow( driver, 'gutenbergSimpleSiteUser' );
+				return await this.loginFlow.loginAndStartNewPost( null, true );
 			} );
 
 			step( 'Can enter post title and content', async function() {
-				let editorPage = await EditorPage.Expect( driver );
-				await editorPage.enterTitle( blogPostTitle );
-				await editorPage.enterContent( blogPostQuote );
+				const gEditorComponent = await GutenbergEditorComponent.Expect( driver );
+				await gEditorComponent.removeNUXNotice();
+				await gEditorComponent.enterTitle( blogPostTitle );
+				await gEditorComponent.enterText( blogPostQuote );
+				return await gEditorComponent.ensureSaved();
 			} );
 
 			step( 'Can disable sharing buttons', async function() {
-				let postEditorSidebarComponent = await PostEditorSidebarComponent.Expect( driver );
-				await postEditorSidebarComponent.expandSharingSection();
-				await postEditorSidebarComponent.setSharingButtons( false );
-				await postEditorSidebarComponent.closeSharingSection();
+				return await SlackNotifier.warn(
+					'Sharing buttons not currently available for Gutenberg in wp-admin'
+				);
+				//let postEditorSidebarComponent = await PostEditorSidebarComponent.Expect( driver );
+				//await postEditorSidebarComponent.expandSharingSection();
+				//await postEditorSidebarComponent.setSharingButtons( false );
+				//await postEditorSidebarComponent.closeSharingSection();
 			} );
 
 			step( 'Can allow comments', async function() {
-				let postEditorSidebarComponent = await PostEditorSidebarComponent.Expect( driver );
-				await postEditorSidebarComponent.expandMoreOptions();
-				await postEditorSidebarComponent.setCommentsForPost( true );
+				const gEditorComponent = await GutenbergEditorComponent.Expect( driver );
+				await gEditorComponent.openSidebar();
+				const gEditorSidebarComponent = await GutenbergEditorSidebarComponent.Expect( driver );
+				await gEditorSidebarComponent.selectDocumentTab();
+				await driver.sleep( 3000 );
+				await gEditorSidebarComponent.collapseStatusAndVisibility(); // Status and visibility starts opened
+				await gEditorSidebarComponent.expandDiscussion();
+				return await gEditorSidebarComponent.setCommentsPreference( { allow: true } );
 			} );
 
-			describe( 'Set to private which publishes it', function() {
-				step( 'Ensure the post is saved', async function() {
-					await EditorPage.Expect( driver );
-					const postEditorToolbarComponent = await PostEditorToolbarComponent.Expect( driver );
-					await postEditorToolbarComponent.ensureSaved();
-				} );
-
-				step( 'Can set visibility to private which immediately publishes it', async function() {
-					const postEditorSidebarComponent = await PostEditorSidebarComponent.Expect( driver );
-					await postEditorSidebarComponent.setVisibilityToPrivate();
-					this.postEditorToolbarComponent = await PostEditorToolbarComponent.Expect( driver );
-					await this.postEditorToolbarComponent.waitForSuccessViewPostNotice();
-					await this.postEditorToolbarComponent.viewPublishedPostOrPage();
-				} );
-
-				if ( host === 'WPCOM' ) {
-					describe( 'As a logged in user ', function() {
-						step( 'Can see correct post title', async function() {
-							let viewPostPage = await ViewPostPage.Expect( driver );
-							let postTitle = await viewPostPage.postTitle();
-							assert.strictEqual(
-								postTitle.toLowerCase(),
-								'private: ' + blogPostTitle.toLowerCase(),
-								'The published blog post title is not correct'
-							);
-						} );
-
-						step( 'Can see correct post content', async function() {
-							let viewPostPage = await ViewPostPage.Expect( driver );
-							let content = await viewPostPage.postContent();
-							assert.strictEqual(
-								content.indexOf( blogPostQuote ) > -1,
-								true,
-								'The post content (' +
-									content +
-									') does not include the expected content (' +
-									blogPostQuote +
-									')'
-							);
-						} );
-
-						step( 'Can see comments enabled', async function() {
-							let viewPostPage = await ViewPostPage.Expect( driver );
-							let visible = await viewPostPage.commentsVisible();
-							assert.strictEqual(
-								visible,
-								true,
-								'Comments are not shown even though they were enabled when creating the post.'
-							);
-						} );
-
-						step( "Can't see sharing buttons", async function() {
-							let viewPostPage = await ViewPostPage.Expect( driver );
-							let visible = await viewPostPage.sharingButtonsVisible();
-							assert.strictEqual(
-								visible,
-								false,
-								'Sharing buttons are shown even though they were disabled when creating the post.'
-							);
-						} );
-
-						describe( 'As a non-logged in user ', function() {
-							before( async function() {
-								await driverManager.clearCookiesAndDeleteLocalStorage( driver );
-								await driver.navigate().refresh();
-							} );
-
-							step( "Can't see post at all", async function() {
-								let notFoundPage = await NotFoundPage.Expect( driver );
-								let displayed = await notFoundPage.displayed();
-								assert.strictEqual(
-									displayed,
-									true,
-									'Could not see the not found (404) page. Check that it is displayed'
-								);
-							} );
-						} );
-					} );
-				} else {
-					// Jetpack tests
-					describe( 'As a non-logged in user ', function() {
-						step( "Can't see post at all", async function() {
-							let notFoundPage = await NotFoundPage.Expect( driver );
-							let displayed = await notFoundPage.displayed();
-							assert.strictEqual(
-								displayed,
-								true,
-								'Could not see the not found (404) page. Check that it is displayed'
-							);
-						} );
-					} );
-					//TODO: Log in via SSO and verify content
+			step(
+				'Set to private which publishes it - Can set visibility to private which immediately publishes it',
+				async function() {
+					const gSidebarComponent = await GutenbergEditorSidebarComponent.Expect( driver );
+					await gSidebarComponent.chooseDocumentSetttings();
+					await gSidebarComponent.expandStatusAndVisibility();
+					await gSidebarComponent.setVisibilityToPrivate();
+					const gEditorComponent = await GutenbergEditorComponent.Expect( driver );
+					return await gEditorComponent.waitForSuccessViewPostNotice();
 				}
+			);
+
+			step( 'Can view content', async function() {
+				const gEditorComponent = await GutenbergEditorComponent.Expect( driver );
+				return await gEditorComponent.viewPublishedPostOrPage();
+			} );
+
+			step( 'As a logged in user - Can see correct post title', async function() {
+				let viewPostPage = await ViewPostPage.Expect( driver );
+				let postTitle = await viewPostPage.postTitle();
+				assert.strictEqual(
+					postTitle.toLowerCase(),
+					'private: ' + blogPostTitle.toLowerCase(),
+					'The published blog post title is not correct'
+				);
+			} );
+
+			step( 'Can see correct post content', async function() {
+				let viewPostPage = await ViewPostPage.Expect( driver );
+				let content = await viewPostPage.postContent();
+				assert.strictEqual(
+					content.indexOf( blogPostQuote ) > -1,
+					true,
+					'The post content (' +
+						content +
+						') does not include the expected content (' +
+						blogPostQuote +
+						')'
+				);
+			} );
+
+			step( 'Can see comments enabled', async function() {
+				let viewPostPage = await ViewPostPage.Expect( driver );
+				let visible = await viewPostPage.commentsVisible();
+				assert.strictEqual(
+					visible,
+					true,
+					'Comments are not shown even though they were enabled when creating the post.'
+				);
+			} );
+
+			step( "Can't see sharing buttons", async function() {
+				let viewPostPage = await ViewPostPage.Expect( driver );
+				let visible = await viewPostPage.sharingButtonsVisible();
+				assert.strictEqual(
+					visible,
+					false,
+					'Sharing buttons are shown even though they were disabled when creating the post.'
+				);
+			} );
+
+			step( 'Ensure we are not logggd in', async function() {
+				await driverManager.clearCookiesAndDeleteLocalStorage( driver );
+				await driver.navigate().refresh();
+			} );
+
+			step( "As a non-logged in user - Can't see post at all", async function() {
+				let notFoundPage = await NotFoundPage.Expect( driver );
+				let displayed = await notFoundPage.displayed();
+				assert.strictEqual(
+					displayed,
+					true,
+					'Could not see the not found (404) page. Check that it is displayed'
+				);
 			} );
 		} );
 	} );
