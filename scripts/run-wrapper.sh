6c8fbd14a90700a155f1a9e9b6741f7025ca27c9
Brent Sessions <brent.sessions@automattic.com>
2018-12-04 08:00:45 -0700

    bad variable

--- scripts/run-wrapper.sh
+++ scripts/run-wrapper.sh
@@ -52,15 +52,15 @@ if [ "$CIRCLE_BRANCH" != "master" ]; then
         MAXCOUNT=120  # 5sec retry = Cancel after 10 minutes
         SITEWAITCOUNT=30 # 5sec retry = Stop waiting after 2.5 minutes
 
-        STATUS=$(curl https://hash-$sha.calypso.live/status 2>/dev/null)
+        STATUS=$(curl https://hash-$MATCH_SHA.calypso.live/status 2>/dev/null)
 
         #Curl to start with no matter what
-        echo "Branch status = $STATUS, running curl https://hash-$sha.calypso.live/"
-        curl https://hash-$sha.calypso.live/ >/dev/null 2>&1
+        echo "Branch status = $STATUS, running curl https://hash-$MATCH_SHA.calypso.live/"
+        curl https://hash-$MATCH_SHA.calypso.live/ >/dev/null 2>&1
 
-        STATUS=$(curl https://hash-$sha.calypso.live/status 2>/dev/null)
+        STATUS=$(curl https://hash-$MATCH_SHA.calypso.live/status 2>/dev/null)
 
-        echo "Branch status after initial curl = $STATUS https://hash-$sha.calypso.live/status"
+        echo "Branch status after initial curl = $STATUS https://hash-$MATCH_SHA.calypso.live/status"
 
         until $(echo $STATUS | grep -wqe "Ready\|NeedsPriming" ); do
             if [ $COUNT == $MAXCOUNT ]; then
@@ -68,22 +68,22 @@ if [ "$CIRCLE_BRANCH" != "master" ]; then
                 exit 1
             elif [ $COUNT == $RESETCOUNT ]; then
                 echo "Reached reset timeout, attempting to reset the branch"
-                curl https://hash-$sha.calypso.live/?reset=true >/dev/null 2>&1
+                curl https://hash-$MATCH_SHA.calypso.live/?reset=true >/dev/null 2>&1
             fi
 
             # If it's still showing NotBuilt, then curl the branch directly rather than the status endpoint
             if [ "NotBuilt" == "$STATUS" ]; then
-                echo "Branch status = $STATUS, running curl https://hash-$sha.calypso.live/"
-                curl https://hash-$sha.calypso.live/ >/dev/null 2>&1
+                echo "Branch status = $STATUS, running curl https://hash-$MATCH_SHA.calypso.live/"
+                curl https://hash-$MATCH_SHA.calypso.live/ >/dev/null 2>&1
             fi
 
             sleep 5
-            STATUS=$(curl https://hash-$sha.calypso.live/status 2>/dev/null)
+            STATUS=$(curl https://hash-$MATCH_SHA.calypso.live/status 2>/dev/null)
             ((COUNT++))
-            echo "Branch status now = $STATUS https://hash-$sha.calypso.live/status"
+            echo "Branch status now = $STATUS https://hash-$MATCH_SHA.calypso.live/status"
         done
 
-        SITE=$(curl https://hash-$sha.calypso.live/ 2>/dev/null)
+        SITE=$(curl https://hash-$MATCH_SHA.calypso.live/ 2>/dev/null)
         COUNT=0
         until ! $(echo $SITE | grep -q "DServe Calypso" ); do
             if [ $COUNT == $SITEWAITCOUNT ]; then
@@ -95,8 +95,8 @@ if [ "$CIRCLE_BRANCH" != "master" ]; then
 
             ((COUNT++))
             sleep 5
-            SITE=$(curl https://hash-$sha.calypso.live/ 2>/dev/null)
-            STATUS=$(curl https://hash-$sha.calypso.live/status 2>/dev/null)
+            SITE=$(curl https://hash-$MATCH_SHA.calypso.live/ 2>/dev/null)
+            STATUS=$(curl https://hash-$MATCH_SHA.calypso.live/status 2>/dev/null)
         done
     fi
 fi
