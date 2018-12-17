56ca15d1259496167cbf64c07d9f8d6198440571
Brent Sessions <brent.sessions@automattic.com>
2018-12-17 12:36:47 -0700

    Cleanup and fixing broken test

--- specs/wp-post-editor-spec.js
+++ specs/wp-post-editor-spec.js
@@ -300,7 +300,7 @@ describe( `[${ host }] Editor: Posts (${ screenSize })`, function() {
 
 			step( 'Can log in', async function() {
 				this.loginFlow = new LoginFlow( driver );
-				return await this.loginFlow.loginAndStartNewPost( { useFreshLogin: true } );
+				return await this.loginFlow.loginAndStartNewPost( null, false, { useFreshLogin: true } );
 			} );
 
 			step( 'Can enter post title and content', async function() {
