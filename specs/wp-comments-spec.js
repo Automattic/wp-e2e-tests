d4c294176772dce89cfe17fcea1278abd7c2cf80
Javon <javondavis@users.noreply.github.com>
2018-12-20 11:05:43 -0500

    removed classic editor comments test

--- specs/wp-comments-spec.js
+++ specs/wp-comments-spec.js
@@ -6,8 +6,6 @@ import * as driverManager from '../lib/driver-manager';
 import * as dataHelper from '../lib/data-helper';
 import * as mediaHelper from '../lib/media-helper';
 import LoginFlow from '../lib/flows/login-flow';
-import EditorPage from '../lib/pages/editor-page';
-import PostEditorToolbarComponent from '../lib/components/post-editor-toolbar-component';
 import CommentsAreaComponent from '../lib/pages/frontend/comments-area-component';
 import GutenbergEditorComponent from '../lib/gutenberg/gutenberg-editor-component';
 
@@ -34,43 +32,6 @@ describe( `[${ host }] Comments: (${ screenSize })`, function() {
 	before( async function() {
 		fileDetails = await mediaHelper.createFile();
 		return fileDetails;
-	} );
-
-	describe( 'Commenting and replying to newly created post: @parallel @jetpack', function() {
-		step( 'Can login and create a new post', async function() {
-			await new LoginFlow( driver ).loginAndStartNewPost();
-			const editorPage = await EditorPage.Expect( driver );
-			await editorPage.enterTitle( blogPostTitle );
-			await editorPage.enterContent( blogPostQuote + '\n' );
-		} );
-
-		step( 'Can publish and visit site', async function() {
-			const postEditorToolbar = await PostEditorToolbarComponent.Expect( driver );
-			await postEditorToolbar.ensureSaved();
-			await postEditorToolbar.publishAndViewContent( { useConfirmStep: true } );
-		} );
-
-		step( 'Can post a comment', async function() {
-			const commentArea = await CommentsAreaComponent.Expect( driver );
-			return await commentArea._postComment( {
-				comment: dataHelper.randomPhrase(),
-				name: 'e2eTestName',
-				email: 'e2eTestName@test.com',
-			} );
-		} );
-
-		step( 'Can post a reply', async function() {
-			await driver.sleep( 10000 ); // Wait to not to post too quickly
-			const commentArea = await CommentsAreaComponent.Expect( driver );
-			await commentArea.reply(
-				{
-					comment: dataHelper.randomPhrase(),
-					name: 'e2eTestName',
-					email: 'e2eTestName@test.com',
-				},
-				2
-			);
-		} );
 	} );
 
 	describe( 'Commenting and replying to newly created post in Gutenberg Editor: @parallel @jetpack', function() {
