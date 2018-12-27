4769307832bf5f14b40d86d837b8f23aa0614407
stojdza <ivanstojadinov@gmail.com>
2018-12-27 16:35:22 +0100

    Remove parallel tag

--- specs/wp-plan-purchase-spec.js
+++ specs/wp-plan-purchase-spec.js
@@ -26,7 +26,7 @@ before( async function() {
 	driver = await driverManager.startBrowser();
 } );
 
-describe( `[${ host }] Plans: (${ screenSize }) @parallel @jetpack`, function() {
+describe( `[${ host }] Plans: (${ screenSize }) @jetpack`, function() {
 	this.timeout( mochaTimeOut );
 
 	describe( 'Comparing Plans:', function() {
