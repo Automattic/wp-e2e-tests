ab3d96e91c21c2645c25e31f9f9890ca9e30c610
Brent Sessions <brent.sessions@automattic.com>
2019-01-10 10:43:35 -0700

    update webhook

--- scripts/notify-webhook.sh
+++ scripts/notify-webhook.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set +o errexit
+
+buildBranch=$BRANCHNAME
+
+if [ "$BRANCHNAME" == null ]; then
+    buildBranch="$E2E_BRANCH"
+fi
+
+
 curl -X POST "https://72e6b435.ngrok.io/circleciwebhook" \
     -H 'Cache-Control: no-cache'                                                   \
     -H 'Content-Type: application/json'                                            \
@@ -9,7 +17,7 @@ curl -X POST "https://72e6b435.ngrok.io/circleciwebhook" \
                 "status": "'"$1"'",
                 "build_url": "'"$CIRCLE_BUILD_URL"'",
                 "build_parameters": {
-                    "branch": "'"$BRANCHNAME"'",
+                    "branch": "'"$buildBranch"'",
                     "build_num": '"$CIRCLE_BUILD_NUM"',
                     "sha": "'"$sha"'",
                     "prContext": "'"$prContext"'",
