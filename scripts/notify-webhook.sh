a80ca9ca071ba0827dea2bd07eb92a7dd72e8476
Brent Sessions <brent.sessions@automattic.com>
2019-01-10 11:01:35 -0700

    Remove unneeded

--- scripts/notify-webhook.sh
+++ scripts/notify-webhook.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set +o errexit
 
-buildBranch=$BRANCHNAME
-
-if [ "$BRANCHNAME" == null ]; then
-    buildBranch="$E2E_BRANCH"
-fi
-
-
 curl -X POST "https://72e6b435.ngrok.io/circleciwebhook" \
     -H 'Cache-Control: no-cache'                                                   \
     -H 'Content-Type: application/json'                                            \
