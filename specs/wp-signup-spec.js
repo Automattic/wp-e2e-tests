857554b4b782517e0344ff24ef906dc4b5151d21
Michael Cain <michael@michaeldcain.com>
2019-02-14 14:34:46 -0500

    Update `improvedOnboarding` test date.

--- specs/wp-signup-spec.js
+++ specs/wp-signup-spec.js
@@ -1647,7 +1647,7 @@ describe( `[${ host }] Sign Up  (${ screenSize }, ${ locale })`, function() {
 		let undo = null;
 
 		before( async function() {
-			undo = overrideABTest( 'improvedOnboarding_20190131', 'onboarding' );
+			undo = overrideABTest( 'improvedOnboarding_20190214', 'onboarding' );
 			return await driverManager.ensureNotLoggedIn( driver );
 		} );
 
@@ -1725,7 +1725,7 @@ describe( `[${ host }] Sign Up  (${ screenSize }, ${ locale })`, function() {
 		let undo = null;
 
 		before( async function() {
-			undo = overrideABTest( 'improvedOnboarding_20190131', 'onboarding' );
+			undo = overrideABTest( 'improvedOnboarding_20190214', 'onboarding' );
 			await driverManager.ensureNotLoggedIn( driver );
 		} );
 
