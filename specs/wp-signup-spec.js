a212c8d19e18f4527887386dd5a0f585de9eb7c8
stojdza <ivanstojadinov@gmail.com>
2019-02-21 23:02:36 +0100

    Remove .only

--- specs/wp-signup-spec.js
+++ specs/wp-signup-spec.js
@@ -1719,7 +1719,7 @@ describe( `[${ host }] Sign Up  (${ screenSize }, ${ locale })`, function() {
 		} );
 	} );
 
-	describe.only( 'Sign up for an account only (no site) then add a site via new onboarding flow @parallel', () => {
+	describe( 'Sign up for an account only (no site) then add a site via new onboarding flow @parallel', () => {
 		const userName = dataHelper.getNewBlogName();
 		const blogName = dataHelper.getNewBlogName();
 		let undo = null;
